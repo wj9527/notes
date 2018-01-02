7d9ebf9c07ecd2579389c69aec0ce6ab6b0418cc
KevinBlandy <747692844@qq.com>
2018-01-02 22:24:19 +0800

    ignore

--- Python/框架-selenium/sm-入门.py
+++ Python/框架-selenium/sm-入门.py
@@ -3,7 +3,17 @@
 ----------------------------
 	* 安装
 		pip install selenium
-	
+
+
+----------------------------
+创建对象					|
+----------------------------
+	# 导入webdriver
+	from selenium import webdriver
+
+	# 创建driver对象,通过executable_path指定phantomjs执行文件路径
+	driver = webdriver.PhantomJS(executable_path='./phantomjs')
+
 ----------------------------
 基本的操作					|
 ----------------------------
@@ -70,30 +80,102 @@
 ----------------------------
 元素获取					|
 ----------------------------
-	# 根据id检索
-	driver.find_element_by_id()
-	# 根据名字检索
-	driver.find_element_by_name()
-	driver.find_element_by_xpath()
-	driver.find_element_by_link_text()
-	driver.find_element_by_partial_link_text()
-	# 根据标签名称检索
-	driver.find_element_by_tag_name()
-	# 根据class名称检索
-	driver.find_element_by_class_name()
-	# 根据css检索
-	driver.find_element_by_css_selector()
+	* 基本的获取
+		# 根据id检索
+		driver.find_element_by_id()
+		# 根据名字检索
+		driver.find_element_by_name()
+		driver.find_element_by_xpath()
+		driver.find_element_by_link_text()
+		driver.find_element_by_partial_link_text()
+		# 根据标签名称检索
+		driver.find_element_by_tag_name()
+		# 根据class名称检索
+		driver.find_element_by_class_name()
+		# 根据css选择器检索
+		driver.find_element_by_css_selector()
+	
+	* 也可以优雅的通过api来获取
+		from selenium.webdriver.common.by import By
+		# 通过 By 枚举来指定要过滤的属性,后面valu指定值
+		element = driver.find_element(by=By.ID, value="coolestWidgetEvah")
+
+		* By.CLASS_NAME
+		* By.TAG_NAME
+		* By.NAME
+		* By.LINK_TEXT
+		* By.CSS_SELECTOR
+		* By.XPATH
+	
+
+----------------------------
+鼠标动作链					|
+----------------------------
+	from selenium import webdriver
+	from selenium.webdriver import ActionChains
+	driver = webdriver.PhantomJS(executable_path='./phantomjs')
+
+	# 获取节点对象
+	photo = driver.find_element_by_id('photo')
+
+	# 通过driver创建Action调用链对象
+	action = ActionChains(driver)
+
+	# 移动鼠标到指定的节点
+	action.move_to_element(photo).perform()
+
+	# 单击指定节点
+	action.move_to_element(photo).click(photo).perform()
+
+	# 双击指定节点
+	action.move_to_element(photo).double_click(photo).perform()
+
+	# 右击指定节点
+	action.move_to_element(photo).context_click(photo).perform()
+
+	# 左键hold住指定节点
+	action.move_to_element(photo).click_and_hold(photo).perform()
+
+	# 把photo节点拖拽到next节点
+	action.drag_and_drop(photo, driver.find_element_by_id('next')).perform()
+
+----------------------------
+表单填充					|
+----------------------------
+	* 已经知道了怎样向文本框中输入文字
+	* 但是有时候我们会碰到<select> </select>标签的下拉框,直接点击下拉框中的选项不一定可行
+	* Selenium专门提供了Select类来处理下拉框
+		# 导入 Select 类
+		from selenium.webdriver.support.ui import Select
+
+		# 找到 name 的选项卡
+		select = Select(driver.find_element_by_name('status'))
+
+		# 根据下拉项的位置选择
+		select.select_by_index(1)
+		# 根据选项的值选择
+		select.select_by_value("0")
+		# 根据option标签文本的值选择
+		select.select_by_visible_text("未审核")
+
+		# 取消选择
+		select.deselect_all()
 
 ----------------------------
 弹窗处理					|
 ----------------------------
 	* 处理页面的alert弹窗
-	alert = driver.switch_to_alert()
+		alert = driver.switch_to_alert()
 
 ----------------------------
 页面切换					|
 ----------------------------
-	driver.switch_to_window('name')
+	* 切面页面
+		driver.switch_to_window('页面名称')
+
+	* 也可以使用 window_handles 方法来获取每个窗口的操作对象
+		for handle in driver.window_handles:
+			driver.switch_to_window(handle)
 
 ----------------------------
 页面前进和后退				|
@@ -101,6 +183,11 @@
 	driver.forward()	# 前进
 	driver.back()		# 后退
 	
+----------------------------
+执行js脚本					|
+----------------------------
+	driver.execute_script('alert("哈哈")')
+
 ----------------------------
 cookie						|
 ----------------------------
@@ -125,5 +212,48 @@ cookie						|
 	* .... ...
 
 	* 显示等待,显示指定某个条件,然后设置最长等待时间,如果超时还未找到元素,抛出异常
-		
+		from selenium import webdriver
+		from selenium.webdriver.common.by import By
+		# WebDriverWait 库,负责循环等待
+		from selenium.webdriver.support.ui import WebDriverWait
+		# expected_conditions 类负责条件触发
+		from selenium.webdriver.support import expected_conditions as expected_conditions
 
+		driver = webdriver.Chrome()
+		driver.get("http://www.xxxxx.com/loading")
+		try:
+			# 页面一直循环，直到 id="myDynamicElement" 出现
+			element = WebDriverWait(driver, 10).until(
+				expected_conditions.presence_of_element_located((By.ID, "myDynamicElement"))
+			)
+		finally:
+			driver.quit()
+	
+	* 程序默认会 0.5s 调用一次来查看元素是否已经生成，如果本来元素就是存在的，那么会立即返回。
+	* 下面是一些内置的等待条件,可以直接调用这些条件,而不用自己写某些等待条件了
+		title_is
+		title_contains
+		presence_of_element_located
+		visibility_of_element_located
+		visibility_of
+		presence_of_all_elements_located
+		text_to_be_present_in_element
+		text_to_be_present_in_element_value
+		frame_to_be_available_and_switch_to_it
+		invisibility_of_element_located
+		element_to_be_clickable – it is Displayed and Enabled.
+		staleness_of
+		element_to_be_selected
+		element_located_to_be_selected
+		element_selection_state_to_be
+		element_located_selection_state_to_be
+		alert_is_present
+
+	* 隐式等待比较简单,就是简单地设置一个等待时间,单位为秒
+		from selenium import webdriver
+		driver = webdriver.Chrome()
+		driver.implicitly_wait(10) # seconds
+		driver.get("http://www.xxxxx.com/loading")
+		myDynamicElement = driver.find_element_by_id("myDynamicElement")
+		
+		* 当然如果不设置,默认等待时间为0
