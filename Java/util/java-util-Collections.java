aa331fd6564e30c6bcd1bff8477cd6a4e4681f19
KevinBlandy <747692844@qq.com>
2018-01-17 18:29:33 +0800

    ignore

--- Java/util/java-util-Collections.java
+++ Java/util/java-util-Collections.java
@@ -9,3 +9,10 @@ Collections-静态字段/方法|
 -------------------------
 	boolean addAll(Collection<? super T> c, T... elements)
 		* 把变长数据都存入c
+	
+	void shuffle(List<?> list);
+		* 随机打乱集合数据
+	
+	void shuffle(List<?> list, Random rnd)
+		* 同上,指定的 Random
+	
