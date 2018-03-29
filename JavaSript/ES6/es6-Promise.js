49f139cad002f1396b2a270ddecd25d95876274c
KevinBlandy <747692844@qq.com>
2018-03-29 17:52:22 +0800

    ignore

---
+++ JavaSript/ES6/es6-Promise.js
@@ -0,0 +1,14 @@
+----------------------------
+Promise						|
+----------------------------
+	1,Promise 的含义
+	2,基本用法
+	3,Promise.prototype.then()
+	4,Promise.prototype.catch()
+	5,Promise.prototype.finally()
+	6,Promise.all()
+	7,Promise.race()
+	8,Promise.resolve()
+	9,Promise.reject()
+	10,应用
+	11,Promise.try()
