fca7567b954729fa42caeb6a458b6f420cad4f9f
KevinBlandy <747692844@qq.com>
2018-05-07 22:37:21 +0800

    ignore

--- Shiro/Shiro-整合Spring.java
+++ Shiro/Shiro-整合Spring.java
@@ -8,18 +8,21 @@ Shiro-Shiro Filter		|
 ------------------------
 	# 在web.xml中配置一个filter
 		<!-- shiro filter -->
-		<filter>
-			<filter-name>shiro</filter-name>
+			<filter>
+			<filter-name>shiroFilter</filter-name>
 			<filter-class>org.springframework.web.filter.DelegatingFilterProxy</filter-class>
+			<init-param>
+				<param-name>targetFilterLifecycle</param-name>
+				<param-value>true</param-value>
+			</init-param>
 		</filter>
+
 		<filter-mapping>
-			<filter-name>shiro</filter-name>
-			<url-pattern>/*</url-pattern>								
-			<dispatcher>REQUEST</dispatcher>
-			<dispatcher>FORWARD</dispatcher>
-			<dispatcher>INCLUDE</dispatcher>
-			<dispatcher>ERROR</dispatcher>
-		</filter-mapping>																	*/
+			<filter-name>shiroFilter</filter-name>
+			<url-pattern>/*</url-pattern>			*/
+		</filter-mapping>
 	
-	# 这个拦截器会代替Shiro的filter
+		* 这个拦截器会代替Shiro的filter
+	
+	# 
 	
