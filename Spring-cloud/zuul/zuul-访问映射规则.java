19f52c2c001385f302038180a8e1c6606bea472b
KevinBlandy <747692844@qq.com>
2018-05-30 21:18:31 +0800

    ignore

---
+++ Spring-cloud/zuul/zuul-访问映射规则.java
@@ -0,0 +1,26 @@
+----------------------------
+访问映射规则				|
+----------------------------
+	# 忽略原始的真实服务名称
+		zuul:
+		 ignored-service: USER
+		 routes:
+		  user.seviceId: USER
+		  user.path: /myuser/**								**/
+		
+		* ignored-service 属性非必须,它的存在就是废除指定服务的原有访问地址
+			* 也就是说,必须使用routes定义的新地址
+			* 如果该值为: * (星号),则表示废除所有的微服务原始访问地址
+
+		* routes 像是一个Map
+		* 里面定义了:
+			路由名称.serviceId: 服务名称
+			路由名称.path: 服务新的访问地址
+	
+
+	# 设置统一公共前缀
+		zuul:
+		 predix: /api
+	
+		* 所有的微服务前面都要加: /api
+	
---
+++ Spring-cloud/zuul/zuul.java
@@ -0,0 +1,41 @@
+----------------------------
+zuul						|
+----------------------------
+	# Zuul包含了对请求的:路由,过滤,代理  ...等核心的主要功能
+	# 路由
+		* 负责把外部请求转发到具体的微服务实例上
+		* 是实现外部访问统一入口的基础
+
+	# 过滤
+		* 负责对请求的处理过程进行干预
+		* 是实现请求校验,服务聚合等功能的基础
+	
+	# Zuul和Eureka整合,将Zuul自身注册为Eureka服务治理下的应用,同时从Eureka中获取其他微服务的消息
+		* 以后访问微服务,都是通过Zuul跳转后获得
+	
+	# 'Zuul服务最终还是会注册进Eureka'
+
+
+	# Maven
+		<!-- https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-zuul -->
+		<dependency>
+			<groupId>org.springframework.cloud</groupId>
+			<artifactId>spring-cloud-starter-zuul</artifactId>
+			<version>1.4.4.RELEASE</version>
+		</dependency>
+
+----------------------------
+zuul-入门体验				|
+----------------------------
+	# 配置
+		* 它也要当作一个服务,注册到Eureka中
+		* 所以,它也需要服务提供者的那一套eureka配置
+	
+	# 驱动注解
+		@EnableZuulProxy
+	
+	# 通过路由进行访问:协议:网关主机:端口:zuul服务名称/接口
+		http://localhost:8081/zuu-service/user/1
+		
+		
+
