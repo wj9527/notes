1978cf7b42587f35f2af8cd921afdd7455dcba04
KevinBlandy <747692844@qq.com>
2018-06-05 13:51:52 +0800

    python3.6.5 pip 踩坑

--- Python/python-基础-pip.py
+++ Python/python-基础-pip.py
@@ -13,3 +13,13 @@ python-pip						|
 	* 模块下载目录(sys.path 中可见)
 		C:\Users\Kevin\AppData\Local\Programs\Python\Python36-32\lib\site-packages
 		
+
+--------------------------------
+python 3.6.5后pip丢失的问题		|
+--------------------------------
+	* 在该版本的环境下执行: pip,提示不是命名
+	* 解决方案,在控制台执行命令
+		python -m ensurepip
+		
+		* 该命令执行成功后会在 script 目录生成pip3等文件
+		* 使用 pip3 代替原来的pip
