990496bbefb35f3cf33df85c57567af57c5106c4
KevinBlandy <747692844@qq.com>
2018-06-12 14:05:07 +0800

    ignore

---
+++ Elasticsearch/es-入门.java
@@ -0,0 +1,18 @@
+-----------------------------
+Elasticsearch			 	 |
+-----------------------------
+	# 官网
+		https://www.elastic.co
+		https://www.elastic.co/cn/
+		https://www.elastic.co/guide/en/elasticsearch/reference/index.html
+
+	# Github
+		https://github.com/elastic/elasticsearch
+	
+	# 参考
+		https://blog.csdn.net/laoyang360/article/details/79293493
+
+	
+	# 传统关系型数据库的对比
+		Relational DB ->	Databases	-> Tables	-> Rows			-> Columns
+		Elasticsearch ->	Indices		-> Types	-> Documents	-> Fields
