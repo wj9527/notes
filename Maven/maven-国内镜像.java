0c6a4d7707a63aeab1a4f9b5a88f6fe94d8649c9
KevinBlandy <747692844@qq.com>
2018-07-04 18:11:22 +0800

    ignore

---
+++ Maven/maven-国内镜像.java
@@ -0,0 +1,9 @@
+
+# »ªÎªÔÆ
+	<mirror>
+		<id>nexus-huaweicloud</id>
+		<mirrorOf>*</mirrorOf>
+		<name>Nexus huaweicloud</name>
+		<url>https://mirrors.huaweicloud.com/repository/maven/</url>
+	</mirror>
+
