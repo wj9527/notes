b7f597e4e6cb0bfe695bd7025733a9ca945fca8e
KevinBlandy <747692844@qq.com>
2018-07-24 11:04:26 +0800

    ignore

--- OkHttp/okhttp-POST.java
+++ OkHttp/okhttp-POST.java
@@ -9,3 +9,19 @@ POST				 |
 		Response response = client.newCall(request).execute();
 		return response.body().string();
 	}
+
+---------------------
+构建普通表单体		 |
+---------------------
+	RequestBody.create(MediaType.parse("application/json; charset=utf-8"), "{}");
+	RequestBody.create(MediaType.parse("application/x-www-form-urlencoded; charset=utf-8"), "name=KevinBlandy");
+
+---------------------
+构建文件表单体		 |
+---------------------
+	File file = new File("");
+	RequestBody requestBody = new MultipartBody.Builder()
+	.setType(MultipartBody.FORM)
+	.addFormDataPart("file",file.getName(), RequestBody.create(MediaType.parse("image/png"), file))
+	.addFormDataPart("name", "KevinBlandy")
+	.build();
