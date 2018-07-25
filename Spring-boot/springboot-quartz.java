68b1c8a43929b901c1605fc7f86e36056f42bd72
KevinBlandy <747692844@qq.com>
2018-07-25 18:06:26 +0800

    ignore

---
+++ Spring-boot/springboot-quartz.java
@@ -0,0 +1,8 @@
+--------------------------------
+quartz	整合					|
+--------------------------------
+	# 导入依赖
+		<dependency>
+		   <groupId>org.springframework.boot</groupId>
+		   <artifactId>spring-boot-starter-quartz</artifactId>
+		</dependency>
