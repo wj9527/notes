beadc00792ea12638c8244b2c0cfae4f76cc69d3
KevinBlandy <747692844@qq.com>
2018-08-09 13:27:48 +0800

    ignore

--- Elasticsearch/es-搜索引擎-mapping.java
+++ Elasticsearch/es-搜索引擎-mapping.java
@@ -30,4 +30,15 @@ mapping				|
 		* ES自动建立mapping的试试,设置了不同的field不同的data type
 		* 不同的data type的数据类型,分词,搜索行为是不一样的,所以出现了 _all,field的搜索表现不一样
 
-		
+	
+	# 透彻理解mapping
+		1,往es里面直接插入数据,es会自动建立索引,同时建立type以及对应的mapping
+		2,mapping中就自动定义了每个field的数据类型
+		3,不同的数据类型(text,data),可能有的是exact value,有的是full text
+		4,exact value 在建立倒排索引,分词的时候,是将整个值一起作为一个关键字建立到倒排索引中的
+		5,full text 就会进过各种处理(分词,normalizationm....),建立在倒排索引中
+		6,exact value 和 full text 的field在被搜索的时候,query string 也会经过相同的行为处理
+			* exact value 全值匹配
+			* full text 先对query string进行分词,normalizationm,再进行匹配
+		7,可以用es的dynamic mapping,让其自动建立mapping(自动设置数据类型),也可以手动的提前为index创建mapping,手动对每个field设置数据类型,索引行为,分词器...
+		8,mapping,就是index的type的元数据,每个type都有一个自己的mapping,决定了数据类型,建立倒排索引的行为,还有进行搜索的行为
--- Elasticsearch/es-搜索引擎-分词器.java
+++ Elasticsearch/es-搜索引擎-分词器.java
@@ -19,4 +19,43 @@
 		4,Language analyzer
 
 	
+-----------------------------
+query String分词器			 |
+-----------------------------
+	# query tring 默认情况下,es会使用它对应的field建立倒排索引时使用的分词器去进行分词
+		* 分词,normalizationm,只有这种才能正确的搜索
+		* query string 也会被进行分词,分词策略就是document建立倒排索引时的分词策略
 	
+	# 不同类型的类型(exact value,full text),不同对待
+		/_search?q=name:KevinBlandy
+		/_all/_search?q=KevinBlandy
+
+-----------------------------
+分词器的测试				 |
+-----------------------------
+	# 请求
+		GET /_analyze
+		{
+			"analyzer": "standard",			//指定分词器
+			"text":"Hello KevinBlandy"		//指定词句
+		}
+
+	# 响应分词信息
+		{
+		  "tokens": [
+			{
+			  "token": "hello",
+			  "start_offset": 0,
+			  "end_offset": 5,
+			  "type": "<ALPHANUM>",
+			  "position": 0
+			},
+			{
+			  "token": "kevinblandy",
+			  "start_offset": 6,
+			  "end_offset": 17,
+			  "type": "<ALPHANUM>",
+			  "position": 1
+			}
+		  ]
+		}
