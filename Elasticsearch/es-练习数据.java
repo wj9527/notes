ac12d489b2f284bba883a8b4456a3fbb83036127
KevinBlandy <747692844@qq.com>
2018-08-10 09:40:09 +0800

    ignore

---
+++ Elasticsearch/es-练习数据.java
@@ -0,0 +1,54 @@
+
+GET /_cat/indices?v
+
+PUT /user?pretty
+
+PUT /user/coder/1?pretty
+{
+	"id":1,
+	"name":"KevinBlandy",
+	"birthday":"2018-8-9 21:23:30",
+	"gender":"ÄÐ",
+	"skill":["Java","Python","Javascript"],
+	"describe":"Christmas Day is on December 25th. It was originated in the western country, but today, this festival has been celebrated by the world. For the manufacturers, they are very happy to make this day as a shopping day. I enjoy the great atmosphere."
+}
+
+PUT /user/coder/2?pretty
+{
+	"id":2,
+	"name":"Litch",
+	"birthday":"2017-7-9 15:15:20",
+	"gender":"ÄÐ",
+	"skill":["Java","C++","Ruby"],
+	"describe":"I had a very special Christmas day last year. I experienced the western style festival. There was a new foreign teacher taught us the lesson. She was about 50 years old and she was very kind and we all liked her. On Christmas Day, she brought us the desserts she made early in the morning. We enjoyed the home-made cakes. What's more, she invited us to came to her house and spent the day with her. Then for the first time, I ate big turkey, which was so delicious. The turkey was filled with many stuffs and the flavor was so good. After dinner, we sang songs and danced."
+}
+
+PUT /user/coder/3?pretty
+{
+	"id":3,
+	"name":"Rocco",
+	"birthday":"1994-8-9 22:15:14",
+	"gender":"ÄÐ",
+	"skill":["C","Go","Groovy","Java"],
+	"describe":"Thanks to my foreign teacher, I experienced the American style festival. It was such funny for me. Though today many people enjoy shopping in all kinds of festivals, the meaning of these festival should be remembered. "
+}
+
+PUT /user/coder/4?pretty
+{
+	"id":4,
+	"name":"Lili",
+	"birthday":"1996-12-9 14:25:30",
+	"gender":"Å®",
+	"skill":["Java","Ruby","Scala"],
+	"describe":"She had been shopping with her Mom in Wal-Mart. She must have been 6 years old, this beautiful brown haired, freckle-faced image of innocence. It was pouring outside. The kind of rain that gushes over the top of rain gutters, so much in a hurry to hit the Earth, it has no time to flow down the spout."
+}
+
+PUT /user/coder/5?pretty
+{
+	"id":5,
+	"name":"Lucy",
+	"birthday":"1997-12-4 4:15:3",
+	"gender":"Å®",
+	"skill":["Groovy","C++","Delphi"],
+	"describe":"We all stood there under the awning and just inside the door of the Wal-Mart. We all waited, some patiently, others irritated, because nature messed up their hurried day. I am always mesmerized by rainfall. I get lost in the sound and sight of the heavens washing away the dirt and dust of the world. Memories of running, splashing so carefree as a child come pouring in as a welcome reprieve from the worries of my day."
+}
