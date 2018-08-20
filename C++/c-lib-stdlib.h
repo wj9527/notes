abb0d60f831bae6167e08fb8d706132946316a28
KevinBlandy <747692844@qq.com>
2018-08-20 16:56:54 +0800

    c

--- C++/c-lib-stdlib.h
+++ C++/c-lib-stdlib.h
@@ -2,7 +2,22 @@
 stdlib							|
 --------------------------------
 
+exit()
+	* 程序退出,参数为 int 值,表示程序的退出状态
+
 system()
+	* 执行系统命令
+	* 返回值 int,该值就是程序的返回值,在不同的平台不一样
+
+fclose()
+fopen()
+freopen()
+fflush()
+
+FILE
+
+NULL
+	* null 
 
 EXIT_SUCCESS = 0
 	* 成功
--- cmd命令.java
+++ cmd命令.java
@@ -8,3 +8,6 @@
 	for /r %i in (*.lastUpdated)do del %i
 
 	* 需要在maven的仓库目录执行
+
+# 使用 pause 指令让控制台 "请按任意键继续。。。"
+
