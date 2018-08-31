94d0e4bfa79228908b817ddf2938d8f5ead32244
KevinBlandy <747692844@qq.com>
2018-08-31 18:12:16 +0800

    ignore

--- https.java
+++ https.java
@@ -81,25 +81,63 @@ https://github.com/Neilpang/acme.sh/wiki/%E8%AF%B4%E6%98%8E
 	2,springboot配置
 		#ssl
 		server.ssl.enabled=true
-		server.ssl.key-store=classpath:ssl/springboot.io.p12
+		server.ssl.key-store=classpath:ssl/springboot.io.keystore
 		server.ssl.key-store-type=PKCS12
 		server.ssl.key-store-password=[key.store的密码]
 
 
-----------------------------------
+------------------------------------
+手动安装							|
+------------------------------------
+	
+1,clone 
+	git clone git@github.com:certbot/certbot.git
 
-_add_a_and_b:
-   push   %ebx
-   mov    %eax, [%esp+8] 
-   mov    %ebx, [%esp+12]
-   add    %eax, %ebx 
-   pop    %ebx 
-   ret  
+2,生成证书
+	* 在当前服务器生成(standalone)建议
+		./letsencrypt-auto certonly --standalone --email [邮箱] -d [域名] -d [域名]
 
-_main:
-   push   3
-   push   2
-   call   _add_a_and_b 
-   add    %esp, 8
-   ret
+	 * 非当前服务器上生成(manual)
+		./letsencrypt-auto certonly --manual --email [邮箱] -d [域名] -d [域名]
 
+		* 需要在服务器的上安装可访问的,脚本提供的文本
+			
+			CjhdUm0L4oQU0ZHg7F7832FtFweUPlRFJs0LxJGx_qg.-ielpqOUtyZI_Q0f9xYi8-Bj57TsuD5y4mGIMxW9GwM			文本
+			http://[域名]/.well-known/acme-challenge/CjhdUm0L4oQU0ZHg7F7832FtFweUPlRFJs0LxJGx_qg			访问地址
+
+	*  创建成功后会在  /etc/letsencrypt/ 下生成证书文件
+
+3,证书续约(需要在生成证书的服务器上进行)
+	./letsencrypt-auto renew
+		
+
+		Saving debug log to /var/log/letsencrypt/letsencrypt.log
+
+		- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		Processing /etc/letsencrypt/renewal/springcloud.io.conf(处理/etc/letsencrypt/renewal/springcloud.io.conf)							
+		- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		Cert not yet due for renewal(证书还没有到期)														
+
+		- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+		The following certs are not due for renewal yet:(下列证书尚未到期续期)									
+		  /etc/letsencrypt/live/springcloud.io/fullchain.pem expires on 2018-11-29 (skipped)
+		No renewals were attempted.(没有尝试更新)
+		- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# nginx配置
+		ssl on;
+		ssl_certificate      /etc/letsencrypt/live/[域名]/fullchain.pem
+		ssl_certificate_key  /etc/letsencrypt/live/[域名]/privkey.pem
+
+# 转换为 tomcat 的格式
+	1,进入目录  /etc/letsencrypt/live/[域名]/ 执行
+		openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out [域名].p12
+
+		* 生成 p12 文件(会输入一次密码)
+
+	2,根据p12 文件生成 keystore 文件
+		keytool -importkeystore -v  -srckeystore [域名].p12 -srcstoretype pkcs12 -srcstorepass [p12文件的密码] -destkeystore [域名].keystore -deststoretype jks -deststorepass [keytroe的密码]
+
+		* 如果提示警告,可以考虑复制警告的命令,再执行一波
+				keytool -importkeystore -srckeystore [域名].keystore -destkeystore [域名].keystore -deststoretype pkcs12
