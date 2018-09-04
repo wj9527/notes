91f6ecf8a8effd4a127ec903326a4ffe915fad16
KevinBlandy <747692844@qq.com>
2018-09-04 16:34:46 +0800

    ignore

---
+++ C/practice/冒泡排序.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define SIZE 10
+
+void sort(int *arr,size_t size){
+	for(int x = 0 ;x < size ; x++){
+		for(int i = x ; i < size ; i++){
+			if (*(arr + x) < *(arr + i)){
+				int temp = *(arr + x);
+				*(arr + x) = *(arr + i);
+				*(arr + i) = temp;
+			}
+		}
+	}
+}
+/**
+ * Í¨¹ýÖ¸ÕëÃ°ÅÝÅÅÐò
+ */
+int main(void){
+
+	int arr[SIZE] = {1,9,8,4,7,3,5,2,0,6,};
+
+	sort(arr,SIZE);
+
+	for(int x = 0 ;x < SIZE ; x++){
+		printf("%d\n",arr[x]);
+	}
+}
