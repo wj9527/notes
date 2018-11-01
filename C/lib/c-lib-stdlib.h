d5f1ba6cfce987800b27c1718d079b32d06cc22f
KevinBlandy <747692844@qq.com>
2018-11-01 15:49:01 +0800

    ignore

--- C/lib/c-lib-stdlib.h
+++ C/lib/c-lib-stdlib.h
@@ -62,3 +62,56 @@ fclose()
 fopen()
 freopen()
 fflush()
+atexit (void (*)(void));
+	* 该函数参数是一个方法指针
+	* 该方法可以多次调用,最少可以放32个回调函数
+	* 在程序退出的时候,会调用该方法(最后添加的先执行)
+	
+	void f1(){
+		printf("第一个执行\n");
+	}
+	void f2(){
+		printf("第二个执行\n");
+	}
+
+	int main(int argc, char **argv) {
+		atexit(&f1);
+		atexit(&f2);
+
+		/*
+			第二个执行
+			第一个执行
+		*/
+		return EXIT_SUCCESS;
+	}
+
+void qsort(void *arr, size_t length, size_t size, int (*)(const void *, const void *));
+	* 快速排序的实现
+	* arr 待排序数组的首元素
+	* lenngth 数组的长度
+	* size 数组元素的大小
+	*  int (*)(const void *v1, const void *v2) 排序方法的指针
+		- 如果v1 > v2 返回整数
+		- 如果v1 < v2 返回负数
+		- 如果v1 = v2 返回0
+	* 排序
+		int compareTo(const void *v1, const void *v2) {
+			//从大到小排序
+			return *((int *) v1) < *((int *) v2);
+		}
+
+		int main(int argc, char **argv) {
+
+			int arr[] = { 1, 5, 4, 7, 8, 2, 3, 9, 6 };
+
+			size_t arrLength = sizeof(arr) / sizeof(arr[0]);
+			size_t itemSize = sizeof(arr[0]);
+
+			qsort(arr, arrLength, itemSize, &compareTo);
+
+			for (int x = 0; x < arrLength; x++) {
+				printf("%d\n", arr[x]);
+			}
+
+			return EXIT_SUCCESS;
+		}
