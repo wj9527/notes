4dd324a98ddd8ffc1e3a9a60ea370a8663d089a4
KevinBlandy <747692844@qq.com>
2018-11-22 16:04:53 +0800

    ignore

--- Spring-boot/springboot-动态数据源.java
+++ Spring-boot/springboot-动态数据源.java
@@ -11,37 +11,56 @@
 ----------------------------
 1,配置文件					|
 ----------------------------
-	jdbc.datasource.masterurl=jdbc:mysql://localhost:3306/community?useUnicode=true&characterEncoding=utf8&autoReconnect=true&allowMultiQueries=true
-	jdbc.datasource.masterusername=root
-	jdbc.datasource.masterpassword=root
+daynamic:
+  datasources:
+    # master库,唯一只能有一个且名称不可修改
+    master:
+      driver-class-name: com.mysql.cj.jdbc.Driver
+      url: jdbc:mysql://127.0.0.1:3306/springcloud?useUnicode=true&characterEncoding=utf8&autoReconnect=true&allowMultiQueries=true&useSSL=false&serverTimezone=GMT%2b8
+      username: KevinBlandy
+      password: 123456
 
-	jdbc.datasource.slaveurl[0]=jdbc:mysql://localhost:3306/community?useUnicode=true&characterEncoding=utf8&autoReconnect=true&allowMultiQueries=true
-	jdbc.datasource.slaveusername[0]=root
-	jdbc.datasource.slavepassword[0]=root
+      public-key: 
+      initialSize: 2
+      maxActive: 50
+      filters: stat,config
+      connectionProperties: druid.stat.mergeSql=true;druid.stat.slowSqlMillis=5000;config.decrypt=true;config.decrypt.key=${daynamic.datasources.master.public-key}
 
-	jdbc.datasource.slaveurl[1]=jdbc:mysql://localhost:3306/community?useUnicode=true&characterEncoding=utf8&autoReconnect=true&allowMultiQueries=true
-	jdbc.datasource.slaveusername[1]=root
-	jdbc.datasource.slavepassword[1]=root
-
-	jdbc.datasource.slaveurl[2]=jdbc:mysql://localhost:3306/community?useUnicode=true&characterEncoding=utf8&autoReconnect=true&allowMultiQueries=true
-	jdbc.datasource.slaveusername[2]=root
-	jdbc.datasource.slavepassword[2]=root
-
-	jdbc.datasource.slaveurl[3]=jdbc:mysql://localhost:3306/community?useUnicode=true&characterEncoding=utf8&autoReconnect=true&allowMultiQueries=true
-	jdbc.datasource.slaveusername[3]=root
-	jdbc.datasource.slavepassword[3]=root
-
-	jdbc.datasource.driverclassname=com.mysql.jdbc.Driver
-	jdbc.datasource.maxactive=20
-	jdbc.datasource.initialsize=10
+    # slave库,名称唯一可以有多个
+    slave_1:
+      driver-class-name: com.mysql.cj.jdbc.Driver
+      url: jdbc:mysql://127.0.0.1:3306/springcloud?useUnicode=true&characterEncoding=utf8&autoReconnect=true&allowMultiQueries=true&useSSL=false&serverTimezone=GMT%2b8
+      username: KevinBlandy
+      password: 123456
+    
+      public-key: 
+      initialSize: 2
+      maxActive: 50
+      filters: stat,config
+      connectionProperties: druid.stat.mergeSql=true;druid.stat.slowSqlMillis=5000;config.decrypt=true;config.decrypt.key=${daynamic.datasources.slave_1.public-key}
+    slave_2:
+      driver-class-name: com.mysql.cj.jdbc.Driver
+      url: jdbc:mysql://127.0.0.1:3306/springcloud?useUnicode=true&characterEncoding=utf8&autoReconnect=true&allowMultiQueries=true&useSSL=false&serverTimezone=GMT%2b8
+      username: KevinBlandy
+      password: 123456
+    
+      public-key: 
+      initialSize: 2
+      maxActive: 50
+      filters: stat,config
+      connectionProperties: druid.stat.mergeSql=true;druid.stat.slowSqlMillis=5000;config.decrypt=true;config.decrypt.key=${daynamic.datasources.slave_2.public-key}
 
 ----------------------------
 2,DataSourceAspect			|
 ----------------------------
-	package com.kevin.boot.web.datasource;
+	import java.lang.reflect.Method;
+	import java.lang.reflect.ParameterizedType;
+	import java.lang.reflect.Type;
+
 	import org.aspectj.lang.JoinPoint;
 	import org.aspectj.lang.annotation.Aspect;
 	import org.aspectj.lang.annotation.Before;
+	import org.aspectj.lang.annotation.Pointcut;
 	import org.aspectj.lang.reflect.MethodSignature;
 	import org.slf4j.Logger;
 	import org.slf4j.LoggerFactory;
@@ -49,65 +68,59 @@
 	import org.springframework.stereotype.Component;
 	import org.springframework.transaction.annotation.Transactional;
 
-	import java.lang.reflect.Method;
-	import java.lang.reflect.ParameterizedType;
-	import java.lang.reflect.Type;
+	import io.springcloud.datasource.DynamicDataSourceHolder;
 
-	/**
-	 * 动态数据源切面,必需保证该切面最先执行
-	 */
-	@Component
+	@Order(-9999)
 	@Aspect
-	@Order(-999)        //务必保证最先执行
-	public class DataSourceAspect {
-		private static final Logger LOGGER = LoggerFactory.getLogger(DataSourceAspect.class);
-		/**
-		 * 在进入Service方法之前执行
-		 * @param point
-		 * @throws NoSuchMethodException
-		 */
-		@Before(value = "execution(* com.kevin.boot.web.service..*.*(..))")
-		public void before(JoinPoint point) throws NoSuchMethodException {
-			//拦截的实体对象
-			Object target = point.getTarget();
-			//拦截的方法名称
-			String methodName = point.getSignature().getName();
-			//拦截的方法参数
-			Object[] args = point.getArgs();
-			//拦截的方法参数类型
-			Class[] parameterTypes = ((MethodSignature)point.getSignature()).getMethod().getParameterTypes();
-			//获取到的目标方法
+	@Component
+	public class DaynamicDatasouceAop {
+		
+		private static final Logger LOGGER = LoggerFactory.getLogger(DaynamicDatasouceAop.class);
+
+		@Pointcut(value = "execution(* io.springcloud.service.*.*(..))")
+		public void service() {
+		}
+
+		@Before("service()")
+		public void beforService(JoinPoint joinPoint) throws NoSuchMethodException, SecurityException {
+			
+			Object target = joinPoint.getTarget();
+			
+			String methodName = joinPoint.getSignature().getName();
+			
+			Object[] args = joinPoint.getArgs();
+			
+			Class<?>[] parameterTypes = ((MethodSignature) joinPoint.getSignature()).getMethod().getParameterTypes();
+			
 			Method method = null;
-			//通过反射获得拦截的method
+			
 			method = target.getClass().getMethod(methodName, parameterTypes);
-			//如果是桥则要获得实际拦截的method
-			if(method.isBridge()){
-				for(int i = 0; i < args.length; i++){
-					//获得泛型类型
-					Class genClazz = this.getSuperClassGenricType(target.getClass(),0);
-					//根据实际参数类型替换parameterType中的类型
-					if(args[i].getClass().isAssignableFrom(genClazz)){
+			
+			if (method.isBridge()) {
+				for (int i = 0; i < args.length; i++) {
+					Class<?> genClazz = getSuperClassGenricType(target.getClass(), 0);
+					if (args[i].getClass().isAssignableFrom(genClazz)) {
 						parameterTypes[i] = genClazz;
 					}
 				}
-				//获得parameterType参数类型的方法
 				method = target.getClass().getMethod(methodName, parameterTypes);
 			}
-			LOGGER.info("当前事务方法  " + methodName);
+			
+			LOGGER.debug("当前事务方法  " + methodName);
+			
 			Transactional transactional = method.getAnnotation(Transactional.class);
+			
 			if(transactional != null && transactional.readOnly()){
-				//读库
-				LOGGER.info("动态数据源 - 读库");
+				LOGGER.debug("动态数据源 - 读库");
 				DynamicDataSourceHolder.markSlave();
 			}else {
-				//写库
-				LOGGER.info("动态数据源 - 写库");
+				LOGGER.debug("动态数据源 - 写库");
 				DynamicDataSourceHolder.markMaster();
 			}
 		}
-		
-		public Class getSuperClassGenricType(Class clazz, int index) {
-			Type genType = clazz.getGenericSuperclass();        // 得到泛型父类
+
+		public Class<?> getSuperClassGenricType(Class<?> clazz, int index) {
+			Type genType = clazz.getGenericSuperclass();
 			if (!(genType instanceof ParameterizedType)) {
 				return Object.class;
 			}
@@ -115,15 +128,13 @@
 			if (!(params[index] instanceof Class)) {
 				return Object.class;
 			}
-			return (Class) params[index];
+			return (Class<?>) params[index];
 		}
 	}
 
 ----------------------------
 3,DynamicDataSource			|
 ----------------------------
-	package com.kevin.boot.web.datasource;
-
 	import org.slf4j.Logger;
 	import org.slf4j.LoggerFactory;
 	import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
@@ -137,136 +148,86 @@
 	import java.util.concurrent.atomic.AtomicInteger;
 
 	/**
-	 * 动态数据源实现
+	 * Created by KevinBlandy on 2017/10/30 14:05
 	 */
 	public class DynamicDataSource extends AbstractRoutingDataSource {
+
 		private static final Logger LOGGER = LoggerFactory.getLogger(DynamicDataSource.class);
-		/**
-		 * 读库的数量
-		 */
-		private Integer slaveCount;
-		
-		// 轮询计数,初始为-1
+
 		private AtomicInteger counter = new AtomicInteger(-1);
-		
-		// 记录读库的key(配置在ioc中)
-		private List<Object> slaveDataSources = new ArrayList<Object>(0) ;
-		
-		/**
-		 * 该方法的返回值,会作为一个Map<String,DataSource>中的key,去获取对应的数据源.
-		 * 所以,每次请求.只要改变这个key,就可以改变当此次请求,在持久层会话中注入的数据源,实现自动的读写分离
-		 */
+
+		private List<Object> slaveDataSources = new ArrayList<>(0);
+
 		@Override
 		protected Object determineCurrentLookupKey() {
-			/**
-			 * 如果是主库,或者是从库数量为0
-			 */
 			Object key = null;
-			if (DynamicDataSourceHolder.isMaster() || this.slaveCount == 0) {
-				key = DynamicDataSourceHolder.getDataSourceKey();
-				return key;
+			if (DynamicDataSourceHolder.isMaster() || this.slaveDataSources.isEmpty()) {
+				key = DynamicDataSourceHolder.MASTER;
+			} else {
+				key = this.getSlaveKey();
 			}
-			key = this.getSlaveKey();
-			LOGGER.info("Datasource key={}",key);
+			LOGGER.debug("动态数据源 dataSourceKey = {}", key);
 			return key;
-			
 		}
-		/**
-		 * 父类实现了InitializingBean,Spring会在Bean初始化完成后回调 afterPropertlesSet方法
-		 * 通过该方法,获取到所有的读库key,存储到集合
-		 */
+
 		@SuppressWarnings("unchecked")
 		@Override
 		public void afterPropertiesSet() {
 			super.afterPropertiesSet();
-			// 由于父类的resolvedDataSources属性是私有的子类获取不到，需要使用反射获取
 			Field field = ReflectionUtils.findField(AbstractRoutingDataSource.class, "resolvedDataSources");
-			field.setAccessible(true); // 设置可访问
+			field.setAccessible(true); 
 			try {
-				//传递当前对象,反射执行该字段的get方法,获取到所有的数据源
 				Map<Object, DataSource> resolvedDataSources = (Map<Object, DataSource>) field.get(this);
-				/**
-				 * 读库的数据量等于数据源总数减去写库的数量,写库数量为 1
-				 */
-				this.slaveCount = resolvedDataSources.size() - 1;
 				for (Map.Entry<Object, DataSource> entry : resolvedDataSources.entrySet()) {
-					//遍历所有的key
 					if (DynamicDataSourceHolder.MASTER.equals(entry.getKey())) {
-						//如果是写库,跳过之
 						continue;
 					}
-					//添加写库的key到集合
 					slaveDataSources.add(entry.getKey());
 				}
 			} catch (Exception e) {
-				e.printStackTrace();
 				LOGGER.error("afterPropertiesSet error! ", e);
 			}
 		}
-		
-		/**
-		 * 轮询算法实现
-		 */
+
 		public Object getSlaveKey() {
-			Integer index = counter.incrementAndGet() % slaveCount;
-			if (counter.get() > 9999) { 	// 以免超出Integer范围
-				counter.set(-1);		 	// 还原
+			Integer index = counter.incrementAndGet() % this.slaveDataSources.size();
+			if (counter.get() > 9999) {
+				counter.set(-1);
 			}
 			return slaveDataSources.get(index);
 		}
 	}
 
+
 ----------------------------
 3,DynamicDataSourceHolder	|
 ----------------------------
-	package com.kevin.boot.web.datasource;
-
-	/**
-	 * ThreadLocal记录当前线程的DB key
-	 */
 	public class DynamicDataSourceHolder {
-		//写库标识
-		public static final String MASTER = "MASTER";
-		
-		//读库标识
-		private static final String SLAVE = "SLAVE";
-		
-		//使用ThreadLocal记录当前线程的数据源标识
-		private static final ThreadLocal<String> holder = new ThreadLocal<String>();
-		
-		/**
-		 * 设置数据源标识
-		 */
+
+		public static final String MASTER = "master";
+
+		private static final String SLAVE = "slave";
+
+		private static final ThreadLocal<String> HOLDER = new ThreadLocal<>();
+
 		private static void putDataSourceKey(String key) {
-			holder.set(key);
+			HOLDER.set(key);
 		}
-		
-		/**
-		 * 获取数据源key
-		 */
+
 		public static String getDataSourceKey() {
-			return holder.get();
+			return HOLDER.get();
 		}
 		
-		/**
-		 * 标记写库
-		 */
-		public static void markMaster(){
+		public static void markMaster() {
 			putDataSourceKey(MASTER);
 		}
-		
-		/**
-		 * 标记读库
-		 */
-		public static void markSlave(){
+
+		public static void markSlave() {
 			putDataSourceKey(SLAVE);
 		}
-		
-		/**
-		 * 是否是主库
-		 */
+
 		public static boolean isMaster() {
-			return  MASTER.equals(holder.get());
+			return MASTER.equals(HOLDER.get());
 		}
 	}
 
@@ -274,180 +235,100 @@
 ----------------------------
 4,DataSourceConfig			|
 ----------------------------
-	package com.kevin.boot.web.config;
+	import java.util.HashMap;
+	import java.util.Map;
 
-	import com.alibaba.druid.pool.DruidDataSource;
 	import org.springframework.boot.context.properties.ConfigurationProperties;
 
-	import javax.sql.DataSource;
-	import java.util.ArrayList;
-	import java.util.List;
+	import com.alibaba.druid.pool.DruidDataSource;
 
-	/**
-	 * Created by KevinBlandy on 2017/2/28 14:07
-	 * 数据库配置,他妈的说好的 "属性宽松的规则" 呢？
-	 */
-	@ConfigurationProperties(prefix = "jdbc.datasource",ignoreInvalidFields = false)
-	public class DataSourceConfig {
-		private String masterurl;
-		private String masterusername;
-		private String masterpassword;
+
+	@ConfigurationProperties(prefix = "daynamic")
+	public class DynamicDataSourceConfig {
 		
-		private List<String> slaveurl = new ArrayList<String>();
-		private List<String> slaveusername = new ArrayList<String>();
-		private List<String> slavepassword = new ArrayList<String>();
+		private Map<String,DruidDataSource> datasources = new HashMap<>();
 		
-		private String driverclassname;
-		private int maxactive;
-		private int initialsize;
-		
-		public String getMasterurl() {
-			return masterurl;
+		public Map<String,DruidDataSource> getDatasources() {
+			return datasources;
 		}
-		
-		public void setMasterurl(String masterurl) {
-			this.masterurl = masterurl;
-		}
-		
-		public String getMasterusername() {
-			return masterusername;
-		}
-		
-		public void setMasterusername(String masterusername) {
-			this.masterusername = masterusername;
-		}
-		
-		public String getMasterpassword() {
-			return masterpassword;
-		}
-		
-		public void setMasterpassword(String masterpassword) {
-			this.masterpassword = masterpassword;
-		}
-		
-		public List<String> getSlaveurl() {
-			return slaveurl;
-		}
-		
-		public void setSlaveurl(List<String> slaveurl) {
-			this.slaveurl = slaveurl;
-		}
-		
-		public List<String> getSlaveusername() {
-			return slaveusername;
-		}
-		
-		public void setSlaveusername(List<String> slaveusername) {
-			this.slaveusername = slaveusername;
-		}
-		
-		public List<String> getSlavepassword() {
-			return slavepassword;
-		}
-		
-		public void setSlavepassword(List<String> slavepassword) {
-			this.slavepassword = slavepassword;
-		}
-		
-		public String getDriverclassname() {
-			return driverclassname;
-		}
-		
-		public void setDriverclassname(String driverclassname) {
-			this.driverclassname = driverclassname;
-		}
-		
-		public int getMaxactive() {
-			return maxactive;
-		}
-		
-		public void setMaxactive(int maxactive) {
-			this.maxactive = maxactive;
-		}
-		
-		public int getInitialsize() {
-			return initialsize;
-		}
-		
-		public void setInitialsize(int initialsize) {
-			this.initialsize = initialsize;
-		}
-		
-		/**
-		 * 根据配置获取Master连接池对象
-		 * @return
-		 */
-		public DataSource getMasterDataSource(){
-			DruidDataSource masterDataSource = new DruidDataSource();
-			masterDataSource.setUrl(this.getMasterurl());
-			masterDataSource.setUsername(this.getMasterusername());
-			masterDataSource.setPassword(this.getMasterpassword());
-			masterDataSource.setMaxActive(this.getMaxactive());
-			masterDataSource.setInitialSize(this.getInitialsize());
-			masterDataSource.setDriverClassName(this.getDriverclassname());
-			return masterDataSource;
+
+		public void setDatasources(Map<String,DruidDataSource> datasources) {
+			this.datasources = datasources;
 		}
 	}
+
 
 
 ----------------------------
 5,DataSourceAutoConfiguration|
 ----------------------------
-	package com.kevin.boot.web.configuration;
+	import java.sql.SQLException;
+	import java.util.HashMap;
+	import java.util.Map;
 
-	import com.alibaba.druid.pool.DruidDataSource;
-	import com.kevin.boot.web.config.DataSourceConfig;
-	import com.kevin.boot.web.datasource.DynamicDataSource;
-	import com.kevin.boot.web.utils.GeneralUtils;
+	import org.slf4j.Logger;
+	import org.slf4j.LoggerFactory;
 	import org.springframework.beans.factory.annotation.Autowired;
 	import org.springframework.boot.context.properties.EnableConfigurationProperties;
 	import org.springframework.context.annotation.Bean;
 	import org.springframework.context.annotation.Configuration;
 
-	import javax.sql.DataSource;
-	import java.util.HashMap;
-	import java.util.Map;
+	import com.alibaba.druid.pool.DruidDataSource;
+
+	import io.springcloud.datasource.DynamicDataSource;
+	import io.springcloud.datasource.DynamicDataSourceConfig;
+	import io.springcloud.datasource.DynamicDataSourceHolder;
 
 	/**
-	 * Created by KevinBlandy on 2017/2/28 14:11
+	 * 
+	 * 读写分离数据源
+	 * @author KevinBlandy
+	 *
 	 */
+
 	@Configuration
-	@EnableConfigurationProperties(DataSourceConfig.class)
-	public class DataSourceAutoConfiguration {
+	@EnableConfigurationProperties(DynamicDataSourceConfig.class)
+	public class DynamicDataSourceConfiguration {
+		
+		private static final Logger LOGGER = LoggerFactory.getLogger(DynamicDataSourceConfiguration.class);
 		
 		@Autowired
-		private DataSourceConfig dataSourceConfig;
+		private DynamicDataSourceConfig dynamicDataSourceConfig;
 		
 		@Bean
-		public DataSource dataSource(){
+		public DynamicDataSource dynamicDataSource() throws SQLException {
+			
 			DynamicDataSource dynamicDataSource = new DynamicDataSource();
-			//从配置获取一个主库
-			DataSource masterDataSource = this.dataSourceConfig.getMasterDataSource();
-			//默认库为主库
-			dynamicDataSource.setDefaultTargetDataSource(masterDataSource);
-			Map<Object,Object> dataSources = new HashMap<Object,Object>();
-			if(!GeneralUtils.isEmpty(this.dataSourceConfig.getSlaveurl()) && !GeneralUtils.isEmpty(this.dataSourceConfig.getSlaveusername()) && !GeneralUtils.isEmpty(this.dataSourceConfig.getSlavepassword())){
-				int length = this.dataSourceConfig.getSlaveurl().size();
-				DruidDataSource druidDataSource = null;
-				for (int x = 0;x < length ; x++){
-					String url = this.dataSourceConfig.getSlaveurl().get(x);
-					String userName = this.dataSourceConfig.getSlaveusername().get(x);
-					String passWord = this.dataSourceConfig.getSlavepassword().get(x);
-					if(!GeneralUtils.isEmpty(url) && !GeneralUtils.isEmpty(userName) && !GeneralUtils.isEmpty(passWord)){
-						//添加slave库
-						druidDataSource = new DruidDataSource();
-						druidDataSource.setUrl(url);
-						druidDataSource.setUsername(userName);
-						druidDataSource.setPassword(passWord);
-						druidDataSource.setMaxActive(this.dataSourceConfig.getMaxactive());
-						druidDataSource.setInitialSize(this.dataSourceConfig.getInitialsize());
-						dataSources.put("SLAVE"+x,druidDataSource);
-					}
+			
+			Map<Object, Object> dataSources = new HashMap<>();
+			
+			for(Map.Entry<String, DruidDataSource> entry : dynamicDataSourceConfig.getDatasources().entrySet()) {
+				
+				String key = entry.getKey();
+				
+				DruidDataSource dataSource = entry.getValue();
+				
+				if(!key.equals(DynamicDataSourceHolder.MASTER)) {
+					dataSources.put(key, dataSource);
 				}
+				
+				dataSource.init();
+				
+				LOGGER.info("数据源初始化:key={}",key);
 			}
-			//添加主库到集合,注意此处 MASTER 值
-			dataSources.put("MASTER",masterDataSource);
+			
+			DruidDataSource masterDataSource = dynamicDataSourceConfig.getDatasources().get(DynamicDataSourceHolder.MASTER);
+			
+			if(masterDataSource == null) {
+				throw new IllegalArgumentException("未定义,master库(" + DynamicDataSourceHolder.MASTER + ")");
+			}
+			
+			dataSources.put(DynamicDataSourceHolder.MASTER, masterDataSource);
+			
+			dynamicDataSource.setDefaultTargetDataSource(masterDataSource);
 			dynamicDataSource.setTargetDataSources(dataSources);
+			
 			return dynamicDataSource;
 		}
 	}
+
