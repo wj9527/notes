edf7f694d0dbfe243fb53eae1f26debccfd55fe7
KevinBlandy <747692844@qq.com>
2018-12-03 16:13:16 +0800

    ignoe

--- Java/util/java-util-Arrays.java
+++ Java/util/java-util-Arrays.java
@@ -14,3 +14,8 @@ Arrays-静态方法		|
 					* 把可变长数组转换为LIST集合
 	Stream<T>	stream(T...t);
 					* 转换为流
+
+	T[]			copyOf(T[] original, int newLength) 
+					* 扩充原数组,original ,newLength指定新的长度
+					* 返回新的数组,并且会把旧数组的元素复制到新数组,空出来的位置填充默认值
+				
