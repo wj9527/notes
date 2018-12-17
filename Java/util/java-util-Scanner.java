88e5b9da0d35df7d4aef2293c271c977ebc98ff6
KevinBlandy <747692844@qq.com>
2018-12-17 18:22:29 +0800

    ignore

--- Java/util/java-util-Scanner.java
+++ Java/util/java-util-Scanner.java
@@ -4,16 +4,25 @@ Scanner				|
 	# 扫描器
 	# 我用得最多的就是读取屏幕的输入(会阻塞线程)
 		* 还可以用于读取文件,字符等等....
-	# 构造 & 方法
-		Scanner scanner = new Scanner(System.in);
-
-		scanner.nextInt();			
-		scanner.next();
-		scanner.nextLine();
-	
+	# 构造方法
+		Scanner(InputStream in);
+		Scanner(Readable source);
+		Scanner(InputStream source, String charsetName)	
+		Scanner(File source)
 	
 	# jdk1.5以前读取屏幕输入
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         bufferedReader.readLine();
 
-		
+--------------------
+Scanner	- 方法		|
+--------------------
+	int nextInt();			
+	String next();
+	String line nextLine();
+	boolean hasNext()
+	useDelimiter(String pattern);
+		* 使用指定的正则去分割
+	useLocale(Locale locale)
+		* 使用指定的语言环境
+	
--- 数据结构/集合和映射/集合.java
+++ 数据结构/集合和映射/集合.java
@@ -2,10 +2,22 @@
 集合						|
 ----------------------------
 	# 元素不能重复
-	# 底层可以是树或者链表实现
 	# 接口方法
 		add();
+		remove();
 		contains();
 		size();
 		isEmpty();
 	
+	# 底层可以是二分搜索树或者链表实现
+		* 二分搜索树作为底层实现太简单了,几乎API都不用咋修改,这里不写
+
+	# 链表的实现,其实就是在add方法添加判断是否已经存在的逻辑
+		@Override
+		public boolean add(E e) {
+			if(this.contains(e)) {
+				//如果已经包含,则不添加
+				return false;
+			}
+			return this.add(e);
+		}
