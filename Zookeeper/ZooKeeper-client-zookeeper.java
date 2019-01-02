8fbb4cb9f7e0b9a12426024c267c1e3eb8d04d18
KevinBlandy <747692844@qq.com>
2019-01-02 22:26:39 +0800

    ignore

---
+++ Zookeeper/ZooKeeper-client-zookeeper.java
@@ -0,0 +1,9 @@
+-------------------------------------
+zookeeper								 |
+-------------------------------------
+	# zookeeper自带的客户端是官方提供的,比较底层
+		<dependency>
+		    <groupId>org.apache.zookeeper</groupId>
+		    <artifactId>zookeeper</artifactId>
+		    <version>3.4.13</version>
+		</dependency>
