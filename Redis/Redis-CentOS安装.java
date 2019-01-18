e73c36acfb2b30541faecf1c4e064ae5e8f4325b
KevinBlandy <747692844@qq.com>
2019-01-19 00:47:19 +0800

    ignore

--- Redis/Redis-CentOS安装.java
+++ Redis/Redis-CentOS安装.java
@@ -1,3 +1,6 @@
+------------------------
+4版本的安装				|
+------------------------
 1,官网下载
 	https://redis.io/
 2,解压
@@ -25,6 +28,36 @@
 7,编辑配置
 	vim /etc/init.d/redis
 
+------------------------
+5.x版本的安装			|
+------------------------
+	# 安装必须依赖
+		yum install -y gcc
+
+	# 准备目录
+		mkdir /usr/local/redis
+		mkdir /usr/local/redis/bin
+		mkdir /usr/local/redis/conf
+
+	# 下载源码,解压
+		https://redis.io/
+
+	# 进入目录执行
+		make
+
+	# 复制程序文件到目录
+		cd ./redis-5.0.3/src
+
+		cp ./mkreleasehdr.sh /usr/local/redis/bin
+		cp ./redis-benchmark /usr/local/redis/bin
+		cp ./redis-check-aof /usr/local/redis/bin
+		cp ./redis-check-rdb /usr/local/redis/bin
+		cp ./redis-cli /usr/local/redis/bin
+		cp ./redis-sentinel /usr/local/redis/bin
+		cp ./redis-server /usr/local/redis/bin
+		cp ./redis-trib.rb /usr/local/redis/bin
 	
+	# 复制配置文件到目录
+		cp ./redis-5.0.3/redis.conf /usr/local/redis/conf
 
 	
