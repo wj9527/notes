0742533d83b984b1e360a807ada52b101d5bc3cc
KevinBlandy <747692844@qq.com>
2019-01-21 21:46:59 +0800

    ignore

--- Shell/shell-数据类型-字符串.sh
+++ Shell/shell-数据类型-字符串.sh
@@ -7,18 +7,16 @@
 	# 单引号
 		str='this is a string'
 
-		* 单引号字符串的限制：
-			* 单引号里的任何字符都会原样输出,单引号字符串中的变量是无效的
-				name="KevinBlandy"
-				echo 'Hello ${name}'	-> Hello ${name}
-				
-			* 单引号字串中不能出现单独一个的单引号(对单引号使用转义符后也不行)
-					str='hello, \'' -> unexpected EOF while looking for matching `''
+		* 单引号里的任何字符都会原样输出,单引号字符串中的变量是无效的
+			name="KevinBlandy"
+			echo 'Hello ${name}'	-> Hello ${name}
+			
+		* 单引号字串中不能出现单独一个的单引号(对单引号使用转义符后也不行)
+				str='hello, \'' -> unexpected EOF while looking for matching `''
 
-			* 单引号可成对出现,作为字符串拼接使用
-				name='v'
-				str='hello, '$name' !'	-> hello, v !
-
+		* 单引号可成对出现,作为字符串拼接使用
+			name='v'
+			str='hello, '$name' !'	-> hello, v !
 	
 	# 双引号
 		your_name='vin'
@@ -38,6 +36,9 @@
 		#!/bin/bash
 		name="KevinBlandy"
 		echo ${#name}			-> 11
+
+		S="KevinBlandy"
+		echo "${S} size = ${#S}"	-> 11
 	
 	# 提取子字符串
 		string="KevinBlandy"
@@ -45,10 +46,17 @@
 
 		* ${:x:y} ,x表示开始的角标,y表示取多少个字符
 	
-	# 查找子字符串
-		string="runoob is a great site"
-		echo `expr index "$string" io`  -> 4
+	# 查找子字符串角标
+		#!/bin/bash
+		NAME="KevinBlandy"
+		S="B"
+		index=`expr index ${NAME} ${S}`
+		echo "${index}"	# -> 6
 
+		* 返回的开始位置是从1开始计算的
+		* 如果找不到返回0
+		* 实际上使用的是shell命令
+			expr index "KevinBlandy" "B" 
 
 
 
