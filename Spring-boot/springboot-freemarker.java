4ea815d2105d562677300e3bd0a9e8505e5a0508
KevinBlandy <747692844@qq.com>
2019-02-25 09:32:47 +0800

    ignor

--- Spring-boot/springboot-freemarker.java
+++ Spring-boot/springboot-freemarker.java
@@ -18,7 +18,7 @@ spring:
     content-type: text/html
     # 编码，默认: urf-8
     charset: utf-8
-    # 可以解析的视图列表，数组
+    # 指定使用模板的视图列表，数组
     view-names:
       - views
     # 是否检查模板引擎目录是否存在，默认:true
@@ -29,45 +29,49 @@ spring:
     suffix: .ftl
     # 使用指定的属性访问 request 域中的数据
     request-context-attribute: req
-    # 默认:false
+    # 是否把request域的属性添加到模板引擎，默认:false
     expose-request-attributes: true
-    # 默认:false
+    # 是否把session域的属性添加到模板引擎，默认:false
     expose-session-attributes: true
-    # 默认:false
+    # request的属性是否可以覆盖controller的model的同名项。默认:false
     allow-request-override: true
+    # session的属性是否可以覆盖controller的model的同名项。默认 false
+    allow-session-override: false
     # 默认:true
     expose-spring-macro-helpers: true
-    # 默认 false
-    allow-session-override: false
     # 模板引擎加载目录，默认:classpath:/templates/
     template-loader-path:
       - classpath:/templates/
-    # 默认:true
+    # 是否优先从文件系统加载模板引擎，支持热加载。默认:true
     prefer-file-system-access: true
     # freemarker中Configuration设置的配置。也就是定义在:freemarker.coreConfigurable 中的那些静态变量值
     settings:
       datetime_format: yyyy-MM-dd HH:mm:ss
+
 	
 	# 一般设置
 spring:
   freemarker:
     enabled: true
-    request-context-attribute: request
-    expose-request-attributes: true
-    expose-session-attributes: true
-    suffix: .ftl
+    cache: true
     content-type: text/html
-    # 开发环境模版,模板引擎不缓存
-    cache: false
+    charset: utf-8
+    suffix: .ftl
+    # 访问 request 域属性的前缀
+    request-context-attribute: reqeust
+    # 添加 request 域的属性到model
+    expose-request-attributes: true
+    # 添加 session 域的属性到model
+    expose-session-attributes: true
+    # 允许 request 域的属性覆盖model的属性
+    allow-request-override: true
+    # 允许 session 域的属性覆盖model的属性
+    allow-session-override: false
+    check-template-location: true
     template-loader-path:
       - classpath:/templates/
-    charset: UTF-8
-    template-encoding: UTF-8
     settings:
       datetime_format: yyyy-MM-dd HH:mm:ss
-      # 模板引擎检测模板变化时间差
-      # template_update_delay: 0
-
 ----------------------------------
 配置							  |
 ----------------------------------
