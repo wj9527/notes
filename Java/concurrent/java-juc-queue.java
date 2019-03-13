ccd6490ca50ed44f503c920dc11e497633a4fef0
KevinBlandy <747692844@qq.com>
2019-03-13 10:18:42 +0800

    ignore

---
+++ Java/concurrent/java-juc-queue.java
@@ -0,0 +1,12 @@
+------------------------------------
+Queue								|
+------------------------------------
+	# ÌåÏµ
+		Queue(java.util)
+			|-BlockingQueue
+			|-AbstractQueue
+				|-DelayQueue
+				|-SynchronousQueue
+				|-PriorityBlockingQueue
+				|-LinkedTransferQueue
+				|-LinkedBlockingDeque
