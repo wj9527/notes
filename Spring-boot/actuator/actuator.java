42467dd906a99022275746540188d9831d02d1d3
KevinBlandy <747692844@qq.com>
2019-04-04 21:41:08 +0800

    ignore

---
+++ Spring-boot/actuator/actuator.java
@@ -0,0 +1,19 @@
+----------------------------
+actuator					|
+----------------------------
+	# 参考文档
+		https://blog.csdn.net/alinyua/article/details/80009435
+		https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#production-ready
+	
+	# 依赖
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-actuator</artifactId>
+		</dependency>
+	
+	
+	# 相关的配置类
+		WebEndpointProperties
+
+
+
