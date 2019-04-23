856111047f970745f38b6c80b4c86bd0ecb13afa
KevinBlandy <747692844@qq.com>
2019-04-23 10:55:16 +0800

    ignore

---
+++ Spring-cloud/config/config.java
@@ -0,0 +1,16 @@
+----------------------------
+config						|
+----------------------------
+	# Maven
+        <dependency>
+            <groupId>org.springframework.cloud</groupId>
+            <artifactId>spring-cloud-starter-config</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.springframework.cloud</groupId>
+            <artifactId>spring-cloud-config-server</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.springframework.cloud</groupId>
+            <artifactId>spring-cloud-config-client</artifactId>
+        </dependency>
--- Spring-cloud/zuul/zuul-filter.java
+++ Spring-cloud/zuul/zuul-filter.java
@@ -49,33 +49,18 @@ zuul					|
 
 
 ------------------------
-异常处理				|
+通过配置来禁用过滤器	|
 ------------------------
-	# 使用 SendErrorFilter 来处理
-		SendErrorFilter
-			* 它负责处理异常,但是它是否执行的条件是,上下文中存在异常,并且尚未转发到errorPath
-				@Override
-				public boolean shouldFilter() {
-					RequestContext ctx = RequestContext.getCurrentContext();
-					return ctx.getThrowable() != null && !ctx.getBoolean("sendErrorFilter.ran", false);
-				}
+	zuul.<SimpleClasName>.<filterType>.disable=true
 
-		* 可以在执行过程中设置异常,来触发SendErrorFilter进行异常处理
-			RequestContext requestContext = RequestContext.getCurrentContext();
-			requestContext.setThrowable(new Exception());
-
-		* 也可以添加如下的参数
-			// 错误编码
-			requestContext.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			// 异常对象
-			requestContext.set("error.exception",new Exception());
-			// 错误信息
-			requestContext.set("error.message","错误信息");
-	
-	# 因为在请求的生命周期:pre,post,route 三个阶段中,只要有异常抛出都会进入到erro阶段的处理,所以可以自定义ErrorFilter
-		
+	* zuul.AuthFilter.pre.disable=true
 	
 		
+
+------------------------
+动态Filter				|
+------------------------
+	# 对于请求过滤器的动态加载,需要借助动态的jvm语言:Groovy
 
 ------------------------
 FilterProcessor			|
