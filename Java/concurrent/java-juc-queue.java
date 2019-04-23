c93692e9de9279f763b6cb201160d8ef718ea704
KevinBlandy <747692844@qq.com>
2019-04-23 09:44:34 +0800

    ignore

--- Java/concurrent/java-juc-queue.java
+++ Java/concurrent/java-juc-queue.java
@@ -14,4 +14,13 @@ Queue								|
 				|-PriorityBlockingQueue
 				|-LinkedTransferQueue
 				|-LinkedBlockingDeque
+				|-ArrayBlockingQueue
+					* 才用数组结构实现
+					* 入队出队采用一把锁,导致入队出队相互阻塞,效率低下
+
+				|-LinkedBlockingQueue
+					* 采用单链表的形式实现
+					* 采用两把锁的锁分离技术实现入队出队互不阻塞
+					* 是有界队列,不传入容量时默认为最大int值
+
 				
