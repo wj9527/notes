592f2051ec512ba1854cb9d22a3143acfa81efc8
KevinBlandy <747692844@qq.com>
2019-04-24 16:45:58 +0800

    ignore

--- Spring-boot/springboot-test.java
+++ Spring-boot/springboot-test.java
@@ -9,11 +9,35 @@ springboot的单元测试		|
 		</dependency>
 	
 	# 测试代码
+		import io.springboot.jpa.JpaApplication;
+		import io.springboot.jpa.dto.UserDTO;
+		import io.springboot.jpa.repository.UserRepository;
+		import io.springboot.jpa.service.UserService;
+		import junit.framework.TestCase;
+		import org.junit.Test;
+		import org.junit.runner.RunWith;
+		import org.springframework.beans.factory.annotation.Autowired;
+		import org.springframework.boot.test.context.SpringBootTest;
+		import org.springframework.test.context.junit4.SpringRunner;
+
+
+
 		@RunWith(SpringRunner.class)
-		@SpringBootTest
-		public class Test{
+		// 设置@SpringBootApplication 类
+		@SpringBootTest(classes = JpaApplication.class)
+		public class JpaTest {
+
+			// 可以使用IOC
+			@Autowired
+			private UserRepository userRepository;
+
 			@Test
 			public void test(){
-				
+				//TODO 执行测试代码
+
+				// 断言非空
+				TestCase.assertNotNull();
+				// 断言equals
+				TestCase.assertEquals();
 			}
 		}
