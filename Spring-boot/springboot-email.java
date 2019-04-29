16d5be756a541817a855345b8e0f1e3fc4a30523
KevinBlandy <747692844@qq.com>
2019-04-29 22:45:05 +0800

    ignore

--- Spring-boot/springboot-email.java
+++ Spring-boot/springboot-email.java
@@ -14,16 +14,23 @@ email							|
 # email	config					
 #-------------------------------------
 
-spring.mail.host=smtp.exmail.qq.com
-spring.mail.username=
-spring.mail.password=
-spring.mail.port=25
-spring.mail.default-encoding=UTF-8
-spring.mail.protocol=smtp
-
-spring.mail.properties.mail.smtp.auth=true
-spring.mail.properties.mail.smtp.starttls.enable=true
-spring.mail.properties.mail.smtp.starttls.required=true
+spring:
+  mail:
+    host: smtp.exmail.qq.com
+    username: 
+    password: 
+    port: 587
+    default-encoding: UTF-8
+    protocol: smtp
+    properties:
+      mail:
+        smtp:
+          connectiontimeout: 5000
+          timeout: 3000
+          writetimeout: 5000
+          auth: true
+          starttls:
+            enable: true
 
 
 #--------------------------------------
@@ -31,50 +38,34 @@ spring.mail.properties.mail.smtp.starttls.required=true
 #-------------------------------------
 
 
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
+import javax.mail.internet.MimeUtility;
 
 @Component
-public class MailService {
-
-    private static String MAIL_USER = null;
-
-    static {
-        try {
-			// 发件人/收件人地址包含了中文,需要进行编码
-			// name<address> -> KevinBlabdy<747692844@qq.com>
-            MAIL_USER = javax.mail.internet.MimeUtility.encodeText("springboot中文社区");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
+public class EmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
 
     @Value("${spring.mail.username}")
-    private String from;
+    private String username;
+    
+    @Value("${spring.mail.sender}")
+    private String sender;
 
-    /**
-     * 发送html邮件
-     * @param to
-     * @param title
-     * @param content
-     * @throws MessagingException
-     */
-    public void sendHTMLMail(String to,String title,String content) throws MessagingException {
+    public void sendHTMLMail(String to,String title,String content) throws MessagingException, UnsupportedEncodingException {
         MimeMessage message = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setFrom(new InternetAddress(MAIL_USER + "<" + this.from + ">"));
+        helper.setFrom(new InternetAddress(MimeUtility.encodeText(sender) + "<" + this.username + ">"));
         helper.setTo(to);
         helper.setSubject(title);
         helper.setText(content, true);
