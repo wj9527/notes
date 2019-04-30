6224321e5eb73c0debea9f3d5fb2b7602baf9a26
KevinBlandy <747692844@qq.com>
2019-04-30 14:00:05 +0800

    ignore

--- Freemarker/freemarker-自定义方法.java
+++ Freemarker/freemarker-自定义方法.java
@@ -95,6 +95,17 @@
 
 		
 		* 这设计感觉咋那么瓜皮???
+		* 所有的参数被封装为Freemarker定义的类型 BeanModel 后,都有一个方法 (BeanModel implements TemplateModel )
+
+			Object getAdaptedObject(Class<?> hint)
+
+			* 通过该方法,可以获取到调用时传递的原生对象
+			
+			${localDateTimeFormatter(paste.createDate)}
+
+			StringModel stringModel = (StringModel) arguments.get(0);
+			LocalDateTime localDateTime = (LocalDateTime) stringModel.getAdaptedObject(LocalDateTime.class);
+
 
 ----------------------------
 编码实现					|
