e1a3421c3ce5926243234d22812a6c6e6fa32fd1
KevinBlandy <747692844@qq.com>
2019-05-04 14:23:21 +0800

    ignore

---
+++ Mongodb/mongodb-core-文档.java
@@ -0,0 +1,60 @@
+--------------------------------
+文档							|
+--------------------------------
+
+
+--------------------------------
+基本命令						|
+--------------------------------
+	db.[colelction].insert([document])
+		* 往指定的colelction插入一条或者多条记录
+		* 如果是多条记录,那么参数应该是一个包含了多个document的数组
+		* 如果该集合不在该数据库中, MongoDB 会自动创建该集合并插入文档
+	
+	db.[colelction].insertOne([document]) 
+		* 插入一条记录,参数是一个document
+	
+	db.[colelction].insertMany([document...]) 
+		* 插入多条记录,参数是一个document数组
+			db.collection.insertMany([{"b": 3}, {'c': 4}])
+
+	db.[colelction].save([document]) 
+		* 如果不指定 _id 字段 save() 方法类似于 insert() 方法
+		* 如果指定 _id 字段,则会更新该 _id 的数据
+	
+
+	db.[colelction].find()
+		* 查看指定collection中已插入文档
+	
+
+--------------------------------
+更新							|
+--------------------------------
+	
+	# 更新的语法: query,update,config
+
+		db.[colelction].update([query], [update],   {
+			upsert: true,
+			multi: true,
+			writeConcern: <document>
+		})
+	
+		query
+			* update的查询条件,类似sql update查询内where后面的
+		
+		update
+			* update的对象和一些更新的操作符(如$,$inc...)等,也可以理解为sql update查询内后面的SET
+		
+		* config的配置选项
+			upsert
+				* 可选,这个参数的意思是,如果不存在update的记录,是否插入objNew,true 为插入
+				* 默认是 false 不插入
+			
+			multi
+				* 仅仅更新符合条件的第一个,还是更新所有
+			
+			writeConcern
+				* 抛出异常的级别
+	
+
+
--- Mongodb/mongodb.java
+++ Mongodb/mongodb.java
@@ -5,3 +5,19 @@ mongodb						  |
 		https://mongodb.com/
 		https://docs.mongodb.com/
 		https://github.com/mongodb/mongo
+
+------------------------------
+mongodb	- 目录结构			  |
+------------------------------
+	bin
+		|-mongo.exe
+			* 一个JS的客户端,可以使用js的语法去执行客户端的操作
+
+		|-mongod.exe
+	data
+	log
+		|-mongod.log
+
+------------------------------
+mongodb	- 核心概念			  |
+------------------------------
