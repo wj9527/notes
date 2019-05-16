e96f39793ef379ceb15382026994bfb4f2644492
KevinBlandy <747692844@qq.com>
2019-05-16 18:12:59 +0800

    ignore

--- Kotlin/practice/SocketServer.java
+++ Kotlin/practice/SocketServer.java
@@ -1,15 +1,36 @@
+import java.io.Serializable
 import java.net.InetSocketAddress
 import java.net.StandardSocketOptions
+import java.nio.ByteBuffer
 import java.nio.channels.SelectionKey
 import java.nio.channels.Selector
 import java.nio.channels.ServerSocketChannel
 import java.nio.channels.SocketChannel
 
-fun main(args:Array<String>){
+class Message : Serializable {
 
-    val selector = Selector.open();
+    private val header : ByteBuffer = ByteBuffer.allocate(4)
 
-    val serverSocketChannel = ServerSocketChannel.open();
+    constructor(){}
+
+    fun getHeader() : ByteBuffer{
+        return this.header
+    }
+
+    fun getBodyLength(): Int{
+        if(this.header.hasRemaining()){
+            // 不足四个4字节
+            return -1
+        }
+        return this.header.int
+    }
+}
+
+fun main(){
+
+    val selector = Selector.open()
+
+    val serverSocketChannel = ServerSocketChannel.open()
     serverSocketChannel.bind(InetSocketAddress("0.0.0.0", 1024))
     serverSocketChannel.configureBlocking(false)
     serverSocketChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true)
@@ -18,13 +39,13 @@ fun main(args:Array<String>){
 
     while (selector.select() > 0){
 
-        val selectionKeyIterator = selector . selectedKeys ().iterator();
+        val selectionKeyIterator = selector . selectedKeys ().iterator()
 
         while(selectionKeyIterator.hasNext()){
 
-            val selectionKey = selectionKeyIterator.next();
+            val selectionKey = selectionKeyIterator.next()
 
-            selectionKeyIterator.remove();
+            selectionKeyIterator.remove()
 
             when {
                 selectionKey.isAcceptable -> {
@@ -32,7 +53,7 @@ fun main(args:Array<String>){
                     socketChannel.configureBlocking(false)
                     socketChannel.setOption(StandardSocketOptions.SO_KEEPALIVE, true)
 
-                    socketChannel.register(selector, SelectionKey.OP_READ or SelectionKey.OP_WRITE)
+                    socketChannel.register(selector, SelectionKey.OP_READ or SelectionKey.OP_WRITE, Message())
                     // TODO 广播消息
                     // TODO 会话存储
 
