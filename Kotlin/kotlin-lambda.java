07560f0b0977822af3c80d11d24744251c915da6
KevinBlandy <747692844@qq.com>
2019-05-17 11:57:46 +0800

    ignore

--- Kotlin/kotlin-lambda.java
+++ Kotlin/kotlin-lambda.java
@@ -68,7 +68,9 @@ lambda					 |
 
 		* 当访问了一个外部的非 final 变量的时候, 它的值会被作为 Raf 实例对象的一个属性保存下来, Raf 是 final 修饰的, 可以被轻易的捕捉
 		* 然后 Raf 存储的值, 就可以在 lambda 执行时进行修改
-
+	
+	# Kotlin 的lambda 表达式里面 一般是不支持 this 的
+		
 
 -------------------------
 成员引用				 |
@@ -122,6 +124,14 @@ lambda					 |
 		
 		* 注意点就是, 这个方法引用, 是通过类来获取到的
 	
+	# 也可以引用到扩展方法
+		fun String.foo() = println("Hello")
+		fun main() {
+			// 根据对象, 获取到扩展方法
+			var foo = ""::foo
+			foo()
+		}
+	
 	# Kotlin 1.1 以后, 支持从实例对象获取到方法引用
 		* 从指定的实例获取到的方法引用, 是名花有主的,所以可以直接运行, 而不用手动的去指定运行时的对象
 
@@ -143,4 +153,139 @@ lambda					 |
 
 
 		
+
+	# Kotlin 的 lambda 可以无缝的和 Java api 对接
+		* 把一个 Lmabda 传给 Java 方法
+
+			public class Main {
+				public static void foo(Runnable runnable){
+					new Thread(runnable).start();
+				}
+			}
+
+			fun main() {
+				Main.foo {println("Hello")} // 编译器会自动的吧这个lambda转换为 Runnable 的实现
+			}
+		
+			
+			* 也可以使用 object 这种字面量对象
+			* 但是每次调用, 都会创建一个新的字面量对象
+				Main.foo(object : Runnable {
+					override fun run() {
+						println("Hello World")
+					}
+				})
+			
+	
+	# SAM构造方法,把 lambda 转换为 接口
+		* 啥玩意儿啊这
+			fun callDone() : Runnable {
+				return Runnable { println("done") }
+			}
+			fun main() {
+				var runnable = callDone()
+				runnable.run()
+			}
+			
+			* 我的理解就是把 lambda 这个表达式, 作为一个实现存在,可以赋值给变量
+		
+		* 语法: [类型] [lambda]
+			var runnable = Runnable { println("Hello World") }
+		
+	
+-------------------------
+with / apply			 |
+-------------------------
+	# 带接收者的lambda
+		* 非常好理解, 这TM不就是 Javascript 里面函数的 call/apply 么?
+		* 可以指定 lambda 运行时的 this 指向(上下文)
+
+			fun alphabet () : String {
+				val stingBuilder = StringBuilder()
+				for(letter in 'A'..'Z'){
+					stingBuilder.append(letter)
+				}
+				return stingBuilder.toString()
+			}
+			* 需要多次去使用 stingBuilder 变量名称, 去执行这个对象的方法
+
+			fun alphabetWith () : String {
+				val stingBuilder = StringBuilder()
+				with(stingBuilder, {        
+					for(letter in 'A'..'Z'){
+						this.append(letter)
+					}
+					return toString() // 省略 this, 执行访问到帮到对象的上下文方法
+				})
+			}
+			* 使用 with 绑定了 this 指向的对象, 那么在 lambda 里面可以痛快的使用 this
+			* 也可以省略 this 关键字, 因为上下文已经确定了, 可以直接访问方法
+		
+		* 语法: with ([this对象] , [代码块( lambda 表达式 )])
+	
+	
+	# 其实扩展函数,就是一种带了接收者(执行上下文)的lambda
+		* 编译后, 是一个静态方法, 第一个参数就是执行的对象, 就是上下文
+	
+
+	# With 运算,也是有结果的
+		* 也就是说,这个表达式可以返回一个值
+		* 代码块的最后一行结果, 作为返回值
+		
+		var letters = with(StringBuilder(), {
+			for(letter in 'A'..'Z'){
+				this.append(letter)
+			}
+			toString()
+		})
+	
+	
+	# 方法名冲突的问题
+		class Bar {
+			fun foo (){
+				println("Im Foo In Bar")
+			}
+		}
+
+		class Foo {
+			fun foo (){
+				println("Im Foo In Foo")
+			}
+			fun bar (){
+				with (Bar()){
+					foo()            // Im Foo In Bar
+					this.foo()      // Im Foo In Bar
+					this@Foo.foo() // Im Foo In Foo
+				}
+			}
+		}
+
+		fun main() {
+			var foo = Foo()
+			foo.bar()
+		}
+		
+		* 就是一个 with 的lambda 表达式,在一个类方法中, 使用 this 或者不使用, 调用的方法名, 在当前类中也存在
+		* 如果需要在当前的with中调用类中的同名方法,需要使用: this@[类].[方法]
+	
+
+	# apply
+		* apply 跟 with 的作用都是一样的, 都是为了修改运行时环境
+		* 它被设计为一个扩展方法 , 也就是说很多对象都可以执行这个方法
+			val stringBuilder = StringBuilder().apply {
+				for(letter in 'A'..'Z'){
+					this.append(letter)
+				}
+			}
+
+			fun main() {
+				println(stringBuilder.toString())
+			}
+		
+		* apply,需要对象主动的去执行
+		* apply, 始终会返回接收者对象(上下文)
+
+		* 可以用这种方式, 在创建完毕对象后设置一些初始化属性
+
+	
 
--- Kotlin/kotlin-集合.java
+++ Kotlin/kotlin-集合.java
@@ -7,23 +7,114 @@
 
 		var list = arrayListOf(1,2,3,4);						ArrayList
 
+		var set = setOf()
+			* Collections.singleton()							java.util.Collections$SingletonSet
+
 		var set = hashSetOf(1,2,3,4);							HashSet
 	
-		<A,B> mapOf(pairs:Pair<A,B>)								
+		<A,B> mapOf(pairs:Pair<A,B>)							LinkedHashMap
+			
 
 		var map = hashMapOf(1 to "1", 2 to "2", 3 to "3")		HashMap
-			* 使用 to 关键字,完成 key 和 value的映射
 	
-	# list/set 支持的一些操作(扩展函数)
+	# list/set/map 支持的一些操作(扩展函数)
 		last();
 		min();
 		max();
 	
-	# list 支持的特殊扩展函数
 		withIndex(): Iterable<IndexedValue<T>>
+			* list 数据结构的一个api
 			* 返回一个迭代器, 迭代对象包含了下标和值
 				public data class IndexedValue<out T>(public val index: Int, public val value: T)
+		
+		filter()
+			* 过滤, 返回 true 的将会被留下
+				arrayListOf(1,2,3,4,5).filter {it % 2 == 0} // [2, 4]
+			* 如果执行对象是 map, 那么参数就是一个: entry
+				mapOf("1" to "2").filter { it -> it.key == it.value }
+				mapOf("1" to "2").filter { (k,v) -> k == v}
+		
+		filterValues()
+		filterKeys()
+			* map的key 和 value 过滤器
+		
+		mapValues()
+		mapKeys()
+			* map 结构的消费函数
+				mapOf("1" to "2").mapKeys { println("${it.key},${it.value}") }
+				mapOf("1" to "2").mapValues { println("${it.key},${it.value}") }
+		
+		map()
+			* 一个消费函数 ,py/java都有
+				arrayListOf(1,2,3,4,5).map {it * 2} // [2, 4, 6, 8, 10]
+			
+		
+		all()
+		anly()
+			* 判断函数, 返回 boolean
+			* 如果所有都符合条件/或者是任何一个符合条件, 返沪 true
+			
+
+		find()
+			* 返回匹配成功的第一个元素
+				listOf(1,4,3,4).find {it % 2 ==0} // 4
+		
+		count()
+			* 统计 ,它也支持过滤, 返回符合条件的数量
+				listOf(1,2,3,4).count {it % 2 ==0} // 2
+			
+		groupBy()
+			* 聚合,返回的结果是一个 map<?,List<?>>, 跟 java 的stream一样
+			* 把处理结果一样的数据放到一个集合,处理的结果作为key
+				var result = listOf("a","bb","ccc","d","ee","fff").groupBy { it.length }
+				println(result) // {1=[a, d], 2=[bb, ee], 3=[ccc, fff]}
+		
+		flatMap()
+			* 把结果合并为一个流
+			* 先把每个元素做变换, 然后再合并为一个流
+				arrayOf(
+				Book("Java编程思想", arrayListOf("Kevin","Litch")),
+				Book("Python编程思想", arrayListOf("Ruby","xjp")),
+				Book("Javascript编程思想", arrayListOf("Zy","Litch")),
+				Book("C编程思想", arrayListOf("Kevin","Rocco")))
+
+				.flatMap { it.authors }.forEach {println(it)} // 把所有的作者信息, 都组合成了一个流
+		
+
+		
+
+		* 这些函数之间可以链式调用, 跟 Java8的 Stream 一样
 	
+	# 更为高效的 asSequence 
+		* 一个有问题的代码
+			arrayOf("1","2").map { it.length }.filter { it >= 1 }
+
+			* 执行到 map 的时候, 会创建一个数组
+			* 执行到 filer 的时候, 会创建一个数组
+
+			* 问题在于, 每一次执行都会创建新的数组,如果操作过多, 那么严重影响性能
+		
+		* 可以先把需要操作的元素序列化, 使用方法: asSequence() ,在最后收集结果
+			arrayOf("1","2").asSequence().map { it.length }.filter { it >= 1 }.toList()
+
+			* 在中间不会创建任何的集合, 只有在最后收集的时候才会创建
+			* 这个就跟Java的Stream一摸一样的, 如果最后没有执行收集操作, 那么中间的流处理也不会执行
+			
+		
+		* asSequence 就是 Java 的 .stream()
+		* 这是一个集合类的扩展函数, 这种东西也被成为惰性求值
+	
+	# 创建 sequence
+		* 之前的 sequence 都是通过集合的 asSequence 来获取,
+		* 也可以自己去创建, 类似于py的生成器
+			var sequence = generateSequence(0) {it + 1}
+			var result = sequence.takeWhile { it <= 100 }.sum()
+			println(result) // 5050
+			
+			* generateSequence() 给定一个开始元素, 以及对元素的操作lamdba
+		
+
+
 	
 	# 其他的一些相关函数
 		var pari = Pair(v1,v2)
