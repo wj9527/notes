e3d8743815ccab6adc632350dff964ac7f920dfa
KevinBlandy <747692844@qq.com>
2019-05-20 17:09:48 +0800

    ignore

---
+++ Jvm/jvm-参数.java
@@ -0,0 +1,21 @@
+-verbose:gc -Xms20m -Xmx20m -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError
+-verbose:class -XX:+TraceClassLoading -XX:+TraceClassUnloading
+-verbose:class -XX:+TraceClassLoading -XX:+TraceClassUnloading
+
+-Xms
+-Xmx
+-Xnoclassgc
+
+-verbose:gc
+-verbose:class
+
+-XX:PermSize
+-XX:MetaspaceSize
+-XX:MaxPermSize
+-XX:MaxMetaspaceSize
+-XX:MaxDirectMemorySize
+
+-XX:+HeapDumpOnOutOfMemoryError
+-XX:+PrintGCDetails
+-XX:+TraceClassLoading
+-XX:+TraceClassUnLoading
