2560286e596888ed6577286f41d21db3bc8e128f
KevinBlandy <747692844@qq.com>
2019-05-27 17:42:33 +0800

    ignore

--- Jvm/jvm-工具-visualvm.java
+++ Jvm/jvm-工具-visualvm.java
@@ -7,3 +7,34 @@ visualvm			 |
 	
 	# 它对应用程序的实际性能影响很小, 所以可以直接应用在生产环境中
 
+
+---------------------
+开启 jstatd 远程监控 |
+---------------------
+	# 在服务端JDK的bin目录下新建  jstatd.all.policy 文件
+		* 其实目录可以自己选择
+		* 输入代码用于启动JSTATD:
+
+	　　grant codebase "file:${java.home}/../lib/tools.jar" {
+	　　　　permission java.security.AllPermission;
+	　　};
+
+	# 服务端启动 jstatd 
+		jstatd -J-Djava.security.policy=jstatd.all.policy  -p 1024 &
+
+	# 选项
+		-nr
+			* 当没有找到现有的RMI注册表时, 不会尝试在jstatd进程中创建内部RMI注册表
+
+		-p [port]	
+			* 希望找到RMI注册表的端口号, 或者如果未找到, 则在未指定-nr的情况下创建RMI注册表
+
+		-n [rminame]
+			* 远程RMI对象在RMI注册表中绑定的名称
+			* 默认名称是JStatRemoteHost
+			* 如果多个jstatd服务器在同一主机上启动,通过指定该选项,可以使每个服务器导出的RMI对象的名称唯一, 但是,这样做将需要保证在监视客户端的hostid和vmid中存在唯一的服务器名称
+
+		-J [option]
+			* 将选项传递给java程序
+			* 例如:-J-Xms48m将启动内存设置为48兆字节, -J通过是一个通用的约定执行用Java编写的应用程序的底层VM选项
+
