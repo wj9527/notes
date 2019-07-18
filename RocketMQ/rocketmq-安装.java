e5f138a02f4cb31369fd7716f9ac53cf6949d8d7
KevinBlandy <747692844@qq.com>
2019-07-18 20:06:40 +0800

    ignore

--- RocketMQ/rocketmq-安装.java
+++ RocketMQ/rocketmq-安装.java
@@ -21,3 +21,20 @@
 		# 启动异常修改脚本:runbroker.cmd
 
 			set CLASSPATH=.;%BASE_DIR%conf;"%CLASSPATH%"
+
+
+
+## 默认端口
+mqnamesrv:9876
+mqbroker - mqnamesrv:10909
+mqbroker - client:10911
+
+## 启动
+### 启动namesrc
+nohup ./bin/mqnamesrv -n "0.0.0.0:9876" > /dev/null &
+
+### 修改配置文件，设置本机的ip可以被生产者，消费者访问到
+echo 'brokerIP1=192.168.0.3' > conf/broker.properties
+
+### 启动broker 
+nohup ./bin/mqbroker -n localhost:9876 -c conf/broker.properties autoCreateTopicEnable=true > /dev/null &
