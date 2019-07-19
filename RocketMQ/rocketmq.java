e0e2b18b8470a5b07bfda3399bf43fb2f3ca7822
KevinBlandy <747692844@qq.com>
2019-07-19 19:08:17 +0800

    ignore

--- RocketMQ/rocketmq.java
+++ RocketMQ/rocketmq.java
@@ -6,4 +6,4 @@
 
 		https://help.aliyun.com/document_detail/29532.html
 
-		http://ifeve.com/%E3%80%8Aapache-rocketmq%E7%94%A8%E6%88%B7%E6%8C%87%E5%8D%97%E3%80%8B%E5%AE%98%E6%96%B9%E6%96%87%E6%A1%A3/
+		https://github.com/apache/rocketmq/tree/master/docs/cn
