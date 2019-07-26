7b79d0d83503ee97612e197ed0e1429dd2cffe13
KevinBlandy <747692844@qq.com>
2019-07-26 16:26:59 +0800

    ignore

--- Mybatis/MyBatis-分页插件(2).java
+++ Mybatis/MyBatis-分页插件(2).java
@@ -45,5 +45,97 @@ MyBatis-PageRowBounds分页|
 				"offset":2,				// 当前页码
 				"total":3				// 总记录数量
 			}
+
+
+-------------------------
+MyBatis-PageHelper分页	 |
+-------------------------
+	# 静态方法
+		// 仅仅排序不分页
+		Page<E> PageHelper.orderBy(String string);
+
+		// 开始分页，不检索总记录数
+		Page<E> PageHelper.startPage(1, 100, false);
+	
+	# 实例方法
+		* 支持链式调用
+
+		Page<E> setOrderBy(String orderBy)
+			* 设置排序字段
+
+		void setOrderByOnly(boolean orderByOnly)
+			* 设置是否只排序，不分页
 				
-			
+
+-------------------------
+MyBatis-动态的解析排序字段|
+-------------------------
+	# 动态的解析排序字段
+			/**
+		 * 解析排序字段
+		 * 
+		 * @param sorts
+		 * @param orders
+		 * @return
+		 */
+		public static String order(String[] columns, String[] orders) {
+
+			if (columns == null || columns.length == 0 || orders == null || orders.length == 0) {
+				return "";
+			}
+
+			if (columns.length != orders.length) {
+				// 必须保证每个字段都有设置排序策略
+				throw new ServiceException(HttpStatus.BAD_REQUEST, "排序字段和排序策略必须一一对应");
+			}
+
+			int length = columns.length;
+
+			StringBuilder stringBuilder = new StringBuilder();
+
+			for (int x = 0; x < length; x++) {
+
+				String column = columns[x];
+				String order = orders[x];
+
+				// 判断列名称的合法性，防止SQL注入。只能是【字母，数字，下划线】
+				if (!column.matches("[A-Za-z0-9_]+")) {
+					throw new ServiceException(HttpStatus.BAD_REQUEST, "非法的排序字段名称：" + column);
+				}
+
+				// 驼峰转换为下划线
+				column = humpConversionUnderscore(column);
+
+				order = order.toUpperCase();
+
+				// 判断排序策略的合法性
+				if (!(order.equals("ASC") || order.equals("DESC"))) {
+					throw new ServiceException(HttpStatus.BAD_REQUEST, "非法的排序策略：" + column);
+				}
+
+				if (x != 0) {
+					stringBuilder.append(", ");
+				}
+				stringBuilder.append("`" + column + "` " + order);
+			}
+			return stringBuilder.toString();
+		}
+
+		/**
+		 * 驼峰转换为下划线
+		 * 
+		 * @param value
+		 * @return
+		 */
+		public static String humpConversionUnderscore(String value) {
+			StringBuilder stringBuilder = new StringBuilder();
+			char[] chars = value.toCharArray();
+			for (char charactor : chars) {
+				if (Character.isUpperCase(charactor)) {
+					stringBuilder.append("_");
+					charactor = Character.toLowerCase(charactor);
+				}
+				stringBuilder.append(charactor);
+			}
+			return stringBuilder.toString();
+		}
