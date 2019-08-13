a88e58fc8524bd582a67b507f8c4e4e7fabc322a
KevinBlandy <747692844@qq.com>
2019-08-13 22:46:26 +0800

    ignore

--- Algorithm/排序-插入排序-希尔.py
+++ Algorithm/排序-插入排序-希尔.py
@@ -1,5 +1,8 @@
 # 希尔排序
 	* 它是插入排序经过改进后一个船新版本, 也叫做: 缩小增量排序
+	* 插入排序存在的问题
+		* 当需要插入的数，是较小的数时，后移的次数明显增多，会影响效率
+	
 	* 基本思想: 
 		1. 把记录按照下标的一定增量分组
 			gap = length / 2
@@ -10,7 +13,7 @@
 			if gap = 1 : break
 	
 
-# 代码实现
+# 使用交换的方式，代码实现
 def shellSort(arr):
     gab = len(arr) // 2
 
@@ -34,4 +37,31 @@ shellSort(arr)
 
 print(arr)
 
+# 使用移位的方式，代码实现（性能提高）
+def shellSort(arr):
 
+    gab = len(arr) // 2
+
+    while gab > 0:
+        for i in range(gab, len(arr)):
+            # 从第gab个元素，逐个对其所在的组直接进行插入排序
+            x = gab
+            while x < len(arr):
+                j = x
+                val = arr[x]
+                if arr[j] < arr[j - gab]:
+                    while (j - gab) >= 0 and val < arr[j - gab]:
+                        arr[j] = arr[j - gab]
+                        j -= gab
+                    # 找到了val插入的位置
+                    arr[j] = val
+                x += 1
+        gab = gab // 2
+
+
+arr = [5, 4, 7, 1, 8, 2, 9, 3, 6, 0]
+
+
+shellSort(arr)
+
+print(arr)
