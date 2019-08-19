79e208edb6d2bd9f5f8e595c048573defe440559
KevinBlandy <747692844@qq.com>
2019-08-19 09:33:05 +0800

    ignore

--- GIT/git-配置.java
+++ GIT/git-配置.java
@@ -1,8 +1,19 @@
 
 # 查看所有的配置
-git config --list 
+	git config --list
+		* 列出所有可配置的配置项
+
 	core.symlinks=false
 	core.autocrlf=true
+		* LF 是Linux 下的换行符, 对应ASCII中转义字符\n 表示换行
+		* CRLF 是windows 下的换行符, \r\n 表示回车并换行
+		* 设置换行符的处理方式,可选参数值:
+			true :提交时转换为LF, 检出时转换为CRLF
+			false:提交时转换为LF, 检出时不转换
+			input:提交检出均不转换
+		
+		* 一旦换行符与源仓库的不一致时, git 会认为这次修改是删除后重来的, 这样会给 code review 带来巨大的麻烦
+
 	core.fscache=true
 	color.diff=auto
 	color.status=auto
@@ -18,12 +29,19 @@ git config --list
 	filter.lfs.process=git-lfs filter-process
 	filter.lfs.required=true
 	credential.helper=manager
+
 	user.email=747692844@qq.com
 	user.name=KevinBlandy
 		* 用户的邮件和名称
+	
 	core.quotepath=false
 	color.ui=true
 		* 交互界面的文字带颜色
+	
+	gui.encoding=utf-8
+		* 交互页面的编码
+
+
 
 # 设置配置
 	git config --global [配置项] [配置值]
