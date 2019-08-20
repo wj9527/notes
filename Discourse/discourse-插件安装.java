afcc2bbee43ac7008da0d8881b628679fdca43fe
KevinBlandy <747692844@qq.com>
2019-08-20 12:51:09 +0800

    ignore

---
+++ Discourse/discourse-插件安装.java
@@ -0,0 +1,35 @@
+--------------------------------
+插件安装						|
+--------------------------------
+	# 官方教程
+		https://meta.discourse.org/t/install-plugins-in-discourse/19157
+	
+	# 步骤
+		1. 获取到插件的git地址(github/bitbucket)
+		2. 进入程序目录, 编辑: app.yml
+			cd /var/discourse
+			vim containers/app.yml
+		
+		3. 添加插件地址到hock
+			hooks:
+			  after_code:
+				- exec:
+					cd: $home/plugins
+					cmd:
+					  - git clone https://github.com/discourse/docker_manager.git
+					  - git clone https://github.com/discourse/discourse-spoiler-alert.git
+		4. 重构容器
+			cd /var/discourse
+			./launcher rebuild app
+		
+--------------------------------
+一些插件统计					|
+--------------------------------
+	# 官方的插件统计
+		https://meta.discourse.org/c/plugin
+	
+	# 提问/解决插件
+		https://github.com/discourse/discourse-solved.git
+	
+	# 谷歌广告插件
+		https://github.com/discoursehosting/discourse-adsense.git
