e1368bf5ecf64edff90801668085b1be96d0239c
KevinBlandy <747692844@qq.com>
2019-09-21 16:20:24 +0800

    ignore

--- Python/框架-Flask/flask-web-flask.py
+++ Python/框架-Flask/flask-web-flask.py
@@ -6,11 +6,15 @@ Falsk					 |
 			self,
 			import_name,
 			static_url_path=None,
+				* 访问静态资源的URL
 			static_folder='static',
+				* 静态资源目录
 			static_host=None,
 			host_matching=False,
 			subdomain_matching=False,
 			template_folder='templates',
+				* 模板引擎目录
+
 			instance_path=None,
 			instance_relative_config=False,
 			root_path=None
