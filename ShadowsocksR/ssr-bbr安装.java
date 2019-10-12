d3d5dcb5d9cfa295358b7c11a64306d2a090c626
KevinBlandy <747692844@qq.com>
2019-10-12 11:36:21 +0800

    ignore

--- ShadowsocksR/ssr-bbr安装.java
+++ ShadowsocksR/ssr-bbr安装.java
@@ -1,7 +1,42 @@
-class  
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");
-	}
-}
+
+yum update -y
+
+cat /etc/redhat-release
+	* release数值大于7.3即可
+
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+
+egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
+
+grub2-set-default 0
+
+reboot
+
+uname -r
+	* 查看内核是否已更换为4.9 +
+
+echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
+
+echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
+
+sysctl -p 
+
+sysctl net.ipv4.tcp_available_congestion_control
+	* 验证当前TCP控制算法
+	* 返回值一般是(或者关系)
+		net.ipv4.tcp_available_congestion_control = bbr cubic reno 
+		net.ipv4.tcp_available_congestion_control = reno cubic bbr
+	
+sysctl net.ipv4.tcp_congestion_control
+	* 验证bbr是否启动
+	* 返回值一般为
+		net.ipv4.tcp_congestion_control = bbr
+	
+lsmod | grep bbr
+	* 返回值有 tcp_bbr 模块即说明 bbr 已启动
+		tcp_bbr                20480  9 
+	
+
+
+
+
