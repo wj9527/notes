e9065f676f0457661f64610154e40f22d0211eab
KevinBlandy <747692844@qq.com>
2019-10-15 19:58:58 +0800

    ignore

--- quartz/quartz-core-Job.java
+++ quartz/quartz-core-Job.java
@@ -6,11 +6,20 @@ Job								|
 	# 任务的执行接口
 		void execute(JobExecutionContext context)   throws JobExecutionException;
 	
-	# 每次执行, 都会创建新的 Job 对象
-		* 需要实现类提供一个无参构造器
-		* 而且尽量不要存储设置实例属性, 而是通过: JobDataMap 来设置和存储
+	# 需要实现类提供一个无参构造器
+
+	# 无状态的Job
+		* 每次执行, 都会创建新的 Job 对象以及 JobDataMap 对象执行
+		* 不要存储设置实例属性
+	
+	# 有状态的Job
+		* 在 Job 的实现接口上添加注解: @PersistJobDataAfterExecution
+		* 每次执行, 都会创建新的Job对象, 但是不会创建新的 JobDataMap
+		* 那么可以通过 JobDataMap 存储一些状态 
 		
 	# Job实现的自动注入
+		* 使用默认的JobFactory: 'org.quartz.simpl.PropertySettingJobFactory' 
+		* 会在Job对象创建完毕后, 调用其setter方法, 注入在 JobDataMap 中的属性到实例
 
 --------------------------------
 JobExecutionContext				|
