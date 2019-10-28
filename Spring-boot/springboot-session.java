b73c4308f4030e42a86c8fd511d69f4e704087cd
KevinBlandy <747692844@qq.com>
2019-10-28 14:43:28 +0800

    ignore

--- Spring-boot/springboot-session.java
+++ Spring-boot/springboot-session.java
@@ -268,4 +268,96 @@ FindByIndexNameSessionRepository|
 	
 	# BUG
 		* 使用 redisson 作为redis的客户端(redisson-spring-boot-starter), 发现不能监听到SESSION的过期事件, 版本号: 3.11.4
-		
+
+---------------------------
+动态的设置cookie的实现		|
+----------------------------
+
+# 自定义cookie的序列化实现
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+// @Component
+public class DynamicCookieMaxAgeCookieSerializer extends DefaultCookieSerializer {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DynamicCookieMaxAgeCookieSerializer.class);
+	
+	public static final String COOKIE_MAX_AGE = "cookie.max-age";
+	
+	@Value("${server.servlet.session.cookie.max-age}")
+	private Integer cookieMaxAge;
+	
+	@Override
+	public void writeCookieValue(CookieValue cookieValue) {
+		HttpServletRequest request = cookieValue.getRequest();
+		Object attribute = request.getAttribute(COOKIE_MAX_AGE);
+		if (attribute != null) {
+			cookieValue.setCookieMaxAge((int) attribute);
+		}else {
+			// 默认cookie的生命周期
+			cookieValue.setCookieMaxAge(this.cookieMaxAge);
+		}
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("动态设置cooke.max-age={}", cookieValue.getCookieMaxAge());
+		}
+		super.writeCookieValue(cookieValue);
+	}
+}
+
+# 配置到IOC
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+
+import com.video.manager.spring.session.DynamicCookieMaxAgeCookieSerializer;
+
+@Configuration
+public class SpringSessionConfiguration {
+	
+	@Value("${server.servlet.session.cookie.name}")
+	private String cookieName;
+	
+	@Value("${server.servlet.session.cookie.secure}")
+	private Boolean cookieSecure;
+	
+//	@Value("${server.servlet.session.cookie.max-age}")
+//	private Integer cookieMaxAge;
+	
+	@Value("${server.servlet.session.cookie.http-only}")
+	private Boolean cookieHttpOnly;
+
+	@Value("${server.servlet.session.cookie.same-site}")
+	private String cookieSameSite;
+	
+	@Bean
+	public CookieSerializer cookieSerializer() {
+		DynamicCookieMaxAgeCookieSerializer serializer = new DynamicCookieMaxAgeCookieSerializer();
+		serializer.setCookieName(this.cookieName);
+		// serializer.setCookieMaxAge(this.cookieMaxAge);
+		serializer.setSameSite(this.cookieSameSite);
+		serializer.setUseHttpOnlyCookie(this.cookieHttpOnly);
+		serializer.setUseSecureCookie(this.cookieSecure);
+		return serializer;
+	}
+}
+
+
+
+# 登录的逻辑
+
+// 失效旧会话 & 创建新会话
+httpSession.invalidate();
+httpSession = request.getSession();
+
+if (!remember) {
+	// 非“记住我”的设置下，cookie生命周期设置为-1 & session缓存30分钟
+	request.setAttribute(DynamicCookieMaxAgeCookieSerializer.COOKIE_MAX_AGE, -1);
+	httpSession.setMaxInactiveInterval(60 * 30);
+}
+
