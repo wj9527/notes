9e06ef41e88e8758ec09261d0727603c27077c04
KevinBlandy <747692844@qq.com>
2019-11-22 17:27:36 +0800

    ignore

---
+++ Java/java-新版本.java
@@ -0,0 +1,28 @@
+----------------------------
+java9						|
+----------------------------
+	# 2017-9 月发布
+	# 比较重要的新特性
+		* 模块化系统
+		* jshell
+		* 多版本兼容jar
+		* 接口的私有方法
+		* 钻石操作符的使用升级
+		* 语法改进:try 语句
+		* 下划线使用限制
+		* String 存储结构变更
+		* 便利的集合特性:of
+		* 增强的Stream API()
+		* 多分辨率图形API
+		* 全新的 HTTP 客户端 API
+		* Deprecated 相关API
+		* 智能Java编译工具
+		* 统一jvm日志系统
+		* javadoc的HTML5支持
+		* javascript 引擎升级: Nashorn
+		* Java的动态编译
+	
+	# 参考资料
+		https://www.ibm.com/developerworks/cn/java/the-new-features-of-Java-9/index.html
+	
+
