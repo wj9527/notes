47d26aad7c292f312ae318bcbdd92ec4b02d0caf
KevinBlandy <747692844@qq.com>
2019-11-25 17:51:31 +0800

    ignore

--- Java/util/java-util-Random.java
+++ Java/util/java-util-Random.java
@@ -12,3 +12,15 @@ java.util.Random		 |
 	r.nextInt();					//返回 0 - 1 之间的任何数。
 	r.netxInt(int num);				//返回0 - num之间的随机数。不包含 num.
 		|--给定种子之后。返回的随机数。是相同的。
+	
+	
+	IntStream ints()
+	IntStream ints(long streamSize)
+	IntStream ints(long streamSize, int randomNumberOrigin, int randomNumberBound)
+	IntStream ints(int randomNumberOrigin, int randomNumberBound)
+		* 返回stream流
+		
+		streamSize			流的最大长度
+		randomNumberOrigin	随机数开始
+		randomNumberBound	随机数结束
+
