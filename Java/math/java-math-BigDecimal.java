181156128317f18e60f9e87a3d19c4d025652090
KevinBlandy <747692844@qq.com>
2019-12-06 14:37:10 +0800

    ignore

--- Java/math/java-math-BigDecimal.java
+++ Java/math/java-math-BigDecimal.java
@@ -73,12 +73,43 @@ BigDecimal-实例方法		|
 			roundingMode				指定舍入模式(可以是int或者枚举)
 			mc							通过指定的 MathContext 处理精度和舍入
 	
+	BigDecimal[] divideAndRemainder(BigDecimal divisor) 
+		* 返回的数组包含两个BigDecimal, 分别是商和余数, 其中商总是整数, 余数不会大于除数
+		* 可以利用这个方法判断两个BigDecimal是否是整数倍数
+			BigDecimal n = new BigDecimal("12.75");
+			BigDecimal m = new BigDecimal("0.15");
+			BigDecimal[] dr = n.divideAndRemainder(m);
+			if (dr[1].signum() == 0) {
+				// n是m的整数倍
+			}
+
+	boolean equals(Object x) 
+		* 比较相等的时候, 不仅仅要比较值, 连末尾的0长度也要一样
+			new BigDecimal("123.456").equals(new BigDecimal("123.45600")) // false
+
+	int compareTo(BigDecimal val)
+		* 比较大小
+
+	int scale()
+		* 返回小数位数, 该值包括了最后面的0
+			new BigDecimal("123.4500").scale() // 4
+
 	BigDecimal setScale(int newScale, int roundingMode)
 		* 格式化小数
 			newScale		表示保留的小数位数
 			roundingMode	舍入模式
+		
 	
+	BigDecimal stripTrailingZeros()
+		* 可以将一个BigDecimal格式化为一个相等的, 但去掉了末尾0的BigDecimal
+		* 值不会受到影响, 受到影响的是 scale
 
+	long longValue()
+		* 转换为long, 如果有小数会被丢弃, 超出范围会溢出
+
+	long longValueExact()
+		* 转换成long型, 如果有小数, 或者超出了范围, 会抛出异常	
+	
 	String toString();
 		* 有必要时使用科学计数法
 
