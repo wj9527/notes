a86c1ba5e1f627d5b64810a5a8a31eac4c9292a9
KevinBlandy <747692844@qq.com>
2019-12-23 17:15:41 +0800

    ignore

--- Poste/poste.java
+++ Poste/poste.java
@@ -4,14 +4,23 @@ Poste						 |
 	# 官网
 		https://poste.io
 	
-	# 域名解析
+	# 参考文档
+		https://www.wumvp.com/2019/03/24/poste.html
+	
+	# 域名解析(springcloud.io)
 
-mail.springcloud.io		A		[邮件服务器ip]
-springcloud.io			MX		mail.springcloud.io. 10
-springcloud.io			IN TXT	"v=spf1 mx ~all"
+mail		A		[邮件服务器ip]
 
-[邮件服务器ip]				PTR		mail.springcloud.io
-	* 这没法解析
+smtp		CNAME	mail.springcloud.io	 
+pop			CNAME	mail.springcloud.io	 
+imap		CNAME	mail.springcloud.io
+
+@			MX		mail.springcloud.io
+@			TXT		"v=spf1 mx ~all"
+
+s20191223631._domainkey TXT "DKIM key
+
+
 
 
 	# Docker 安装
@@ -30,9 +39,32 @@ docker run \
 -h "mail.springcloud.io" \
 -t analogic/poste.io
 	
-	# 设置admin邮箱
+	# 设置admin邮箱 & 密码
 		admin@springcloud.io
+	
+	# 创建密钥
+		Virtual domains -> springcloud.io -> create new key
+		
+		* 把 DKIM key 解析到域名
 
 	# SSL设置
 		System settings -> TLS Certificate -> Let's Encrypt certificate
+
+		Enable: √
+		Common name: mail.springcloud.io
+		Alternativenames: 
+			smtp.springcloud.io
+			pop.springcloud.io
+			imap.springcloud.io
+
+
+		* 重启docker服务
+			docker restart PosteServer
 	
+	
+	# 日志
+		* 查看日志
+			docker logs --tail=100 mailserver
+		
+		* 日志目录
+			data/logs
