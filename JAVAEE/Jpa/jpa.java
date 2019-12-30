2d1fa2ad3b707d8ac18b2092a4e828d62ffe7939
KevinBlandy <747692844@qq.com>
2019-12-30 17:14:57 +0800

    ignore

---
+++ JAVAEE/Jpa/jpa.java
@@ -0,0 +1,10 @@
+-----------------------
+jpa
+-----------------------
+	# JavaEE体系中的技术之一
+		<dependency>
+			<groupId>javax.persistence</groupId>
+			<artifactId>javax.persistence-api</artifactId>
+			<version>2.2</version>
+		</dependency>
+	
