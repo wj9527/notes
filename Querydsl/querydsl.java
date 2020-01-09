1b56111d0334d2e3ea86f61afca9398d14b77c8c
KevinBlandy <747692844@qq.com>
2020-01-09 11:07:56 +0800

    ignore

--- Querydsl/querydsl.java
+++ Querydsl/querydsl.java
@@ -3,6 +3,7 @@ querydsl			|
 -------------------
 	# ÎÄµµ
 		http://www.querydsl.com/static/querydsl/latest/reference/html_single/
+		http://www.querydsl.com/static/querydsl/latest/apidocs/
 		https://fududu.gitbooks.io/querydsl-zh_cn/Introduction.html
 	
 	# Maven
