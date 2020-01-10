eccd1092946a54020a9101c064e7880503e36abf
KevinBlandy <747692844@qq.com>
2020-01-10 17:50:04 +0800

    ignore

--- Querydsl/sql/querydsl.java
+++ Querydsl/sql/querydsl.java
@@ -62,3 +62,85 @@ querydsl
 				</plugin>
 			</plugins>
 		</build>
+
+-------------------------------------
+数据表类型和字段类型的映射转换
+-------------------------------------
+	#  通过 configuration 配置转换
+		<typeMappings>
+		  <typeMapping>
+			<table>IMAGE</table>
+			<column>CONTENTS</column>
+			<type>java.io.InputStream</type>
+		  </typeMapping>
+		</typeMappings>
+
+	# 通过 configuration 配置自定义的转换器
+		<customTypes>
+		  <customType>com.querydsl.sql.types.InputStreamType</customType>
+		</customTypes>
+	
+	# 自定义转换器, 实现接口: Type
+		* 泛型就是java类型
+		* 可以一次性把多个jdbc类型映射为同一个java类型
+			interface Type {
+				int[] getSQLTypes();			// sql类型，可以从 Types 获取到
+				Class<T> getReturnedClass();	// java类型
+				String getLiteral(T value);
+				@Nullable
+				T getValue(ResultSet rs, int startIndex) throws SQLException;
+				void setValue(PreparedStatement st, int startIndex, T value) throws SQLException;
+			}
+
+
+		* 也可以直接继承抽象类
+			public abstract class AbstractType<T> implements Type<T> {
+				private final int type;
+				public AbstractType(int type) {
+					this.type = type;
+				}
+				@Override
+				public final int[] getSQLTypes() {
+					return new int[]{type};
+				}
+				@Override
+				public String getLiteral(T value) {
+					return value.toString();
+				}
+			}
+	
+		
+		* demo
+			import java.sql.PreparedStatement;
+			import java.sql.ResultSet;
+			import java.sql.SQLException;
+			import java.sql.Types;
+			import com.querydsl.sql.types.AbstractType;
+			import io.springboot.querydsl.domain.enums.Gender;
+			public class GenderType extends AbstractType<Gender>{
+				public GenderType() {
+					super(Types.TINYINT);
+				}
+				@Override
+				public Class<Gender> getReturnedClass() {
+					return Gender.class;
+				}
+				@Override
+				public Gender getValue(ResultSet rs, int startIndex) throws SQLException {
+					int value = rs.getInt(startIndex);
+					for (Gender gender : Gender.values()) {
+						if (value == gender.ordinal()) {
+							return gender;
+						}
+					}
+					throw new IllegalArgumentException(String.format("枚举：%s 中没有ordinal值=%d的实例", Gender.class.getName(), value));
+				}
+				@Override
+				public void setValue(PreparedStatement st, int startIndex, Gender value) throws SQLException {
+					st.setInt(startIndex, value.ordinal());
+				}
+			}
+
+-------------------------------------
+数据表类型和字段类型的映射转换
+-------------------------------------
