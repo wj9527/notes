144017a8f48ca9660d10c9c5973ccf5458cbeafc
KevinBlandy <747692844@qq.com>
2020-02-29 12:23:29 +0800

    ignore

--- Netty/netty-channel-handler.java
+++ Netty/netty-channel-handler.java
@@ -140,6 +140,16 @@ ChannelOutboundHandler		 |
 				|-MessageToMessageEncoder<I>
 					|-LengthFieldPrepender
 					|-StringEncoder
+	
+	# ChannelOutboundHandler ³éÏó·½·¨
+		void bind(ChannelHandlerContext ctx, SocketAddress localAddress, ChannelPromise promise) throws Exception;
+		void connect( ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) throws Exception;
+		void disconnect(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception;
+		void close(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception;
+		void deregister(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception;
+		void read(ChannelHandlerContext ctx) throws Exception;
+		void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception;
+		void flush(ChannelHandlerContext ctx) throws Exception;
 
 -----------------------------
 ChannelDuplexHandler		 |
