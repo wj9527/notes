14129e8fc77dd2586b16a520dc39b64d6ac8bdf0
KevinBlandy <747692844@qq.com>
2020-03-09 17:57:29 +0800

    ignore

---
+++ Jooq/jooq.java
@@ -0,0 +1,14 @@
+----------------------------
+Jooq
+----------------------------
+	# JOOQ
+		* 全称Java Object Oriented Querying, 即面向Java对象查询
+		* 它是Data Geekery公司研发的DA方案(Data Access Layer), 主要解决两个问题
+
+	# 官方地址
+		https://www.jooq.org/
+		https://github.com/jOOQ/jOOQ
+		https://www.jooq.org/doc/current/manual-single-page/
+
+	# 参考学习
+		https://amao12580.github.io/post/2016/04/JOOQ-from-entry-to-improve/
