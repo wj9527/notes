2146818db0d07021bc275eae70df7eadfd0c9190
KevinBlandy <747692844@qq.com>
2020-03-09 12:53:23 +0800

    ignore

--- Querydsl/jpa/querydsl-结果映射.java
+++ Querydsl/jpa/querydsl-结果映射.java
@@ -46,7 +46,36 @@
 		* 仅仅只会检索指定的字段, 对象必须要有空的构造函数
 		* 会自动根据属性名称封装属性, 如果属性名称不符合, 可以使用别名
 			QUser.user.name.as("name")
-			
+		
+
+		* 多表结果集的情况下, 使用 Projections.bean 封装部分字段, 必须要声明出需要检索的列(属性), 不能直接写一个Q查询对象, 不能没法查询出东西
+
+			// 明星表
+            QStar qStar = QStar.star;
+            
+            // 明星关联的视频表
+            QVideoStar qVideoStar = QVideoStar.videoStar;
+            
+            // 把部分明星数据封装为 StartDTO 对象
+            QBean<StarDTO> qBean = Projections.bean(StarDTO.class, qStar.id, qStar.name, qStar);
+			// QBean<StarDTO> qBean = Projections.bean(StarDTO.class, qStar);	错误
+            
+            JPAQuery<Tuple> jpaQuery = jpaQueryFactory.select(qBean,
+                        JPAExpressions.select(qVideoStar.starId.count()).from(qVideoStar).where(qVideoStar.starId.eq(qStar.id)))
+                    .from(qStar);
+
+            QueryResults<Tuple> queryResults = jpaQuery.fetchResults();
+            List<StarDTO> stars = queryResults.getResults().stream().map(i -> {
+            	
+            	// 获取明星记录数据
+            	StarDTO starDTO = i.get(qBean);
+            	
+            	// 获取子查询统计数据
+                starDTO.setVideoCount(i.get(1, Long.class).intValue());
+                
+                return starDTO;
+            }).collect(Collectors.toList());
+
 	
 
 		
