84c83ee3e80efdc637542b19ca088db8fb90082c
KevinBlandy <747692844@qq.com>
2020-04-19 18:10:41 +0800

    ignore

---
+++ Fiddler/fiddler.java
@@ -0,0 +1,30 @@
+----------------
+fiddler
+----------------
+	# 官网
+		https://www.telerik.com/fiddler
+	
+	# 文档
+		https://docs.telerik.com/fiddler
+
+
+----------------
+fiddler 配置项
+----------------
+	# 修改监听的端口
+		Tools -> Options -> Connections -> Fiddler Listens On Port
+	
+	# 系统启动时, 是否设置系统代理
+		Tools -> Options -> Connections -> Act As SystemProxy On Start Up
+
+
+----------------
+fiddler 快捷键
+---------------
+	r
+		* 快速重复请求
+	
+	shift + r
+		* 弹出输入框, 可以指定重复请求的次数
+	
+	
