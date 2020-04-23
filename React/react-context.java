7858e4c09d4c82f28f06cceb7665bbaf4364b93c
KevinBlandy <747692844@qq.com>
2020-04-23 22:26:53 +0800

    ignore

--- React/react-context.java
+++ React/react-context.java
@@ -90,6 +90,45 @@ context
 			通过 Consumer, 定义函数体来读取到值
 
 
+
+------------------------------
+context - 注意事项
+------------------------------
+	# context 会使用参考标识(reference identity)来决定何时进行渲染, 这里可能会有一些陷阱
+		* 当 provider 的父组件进行重渲染时, 可能会在 consumers 组件中触发意外的渲染
+
+			class App extends React.Component {
+			  render() {
+				return (
+				  <MyContext.Provider value={{something: 'something'}}>
+					<Toolbar />
+				  </MyContext.Provider>
+				);
+			  }
+			}
+
+			* 当每一次 Provider 重渲染时, 以下的代码会重渲染所有下面的 consumers 组件, 因为 value 属性总是被赋值为新的对象
+	
+	# 为了防止这种情况, 将 value 状态提升到父节点的 state 里
+		class App extends React.Component {
+		  constructor(props) {
+			super(props);
+			this.state = {
+			  value: {something: 'something'},
+			};
+		  }
+
+		  render() {
+			return (
+			  <Provider value={this.state.value}>
+				<Toolbar />
+			  </Provider>
+			);
+		  }
+		}
+
+	
+
 ------------------------------
 context - api
 ------------------------------
