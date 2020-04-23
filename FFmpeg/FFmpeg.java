7fa2f632da2be740011a94e7786e454a8d3b3dba
KevinBlandy <747692844@qq.com>
2020-04-23 12:50:22 +0800

    ignore

--- FFmpeg/FFmpeg.java
+++ FFmpeg/FFmpeg.java
@@ -9,5 +9,11 @@ FFmpeg
 		https://github.com/FFmpeg/FFmpeg
 	
 
-	# Windows 安装
-		
+---------------------------
+FFmpeg - 音视频处理流程
+---------------------------
+	1. 输入文件 (demuxer)
+	2. 编码数据包 (decoer)
+	3. 解码后数据帧 
+	4. 编码数据包 (encoder)
+	5. 输出文件 (muxer)
