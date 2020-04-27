cc9ab13f24b1a5030ee1cf7f343b50e7471658a9
KevinBlandy <747692844@qq.com>
2020-04-27 14:08:12 +0800

    ignore

--- Querydsl/jpa/querydsl-子查询.java
+++ Querydsl/jpa/querydsl-子查询.java
@@ -7,7 +7,8 @@
 					JPAExpressions.select(QUser.user.id).from(QUser.user).where(QUser.user.name.eq("KevinBlandy")))
 				)
 				.fetch();
-	
+		
+		* 条件, 单行单列检索
 
 		QUser qUser = QUser.user;
 		queryFactory.select(qUser)
@@ -17,7 +18,26 @@
 					.exists()
 				)
 				.fetch();
-	
+			
+		* 条件, exists检索
+		
+		QCategory qCategory = QCategory.category;
+		QVideoCategory qVideoCategory = QVideoCategory.videoCategory;
+
+		NumberExpression<Long> numberExpression = qVideoCategory.categoryId.count();
+		
+		QBean<CategoryDTO> categoryQBean = Projections.bean(CategoryDTO.class, 
+				qCategory.id, qCategory.title, qCategory.poster, qCategory.describe, qCategory.sorted, qCategory.createdDate);
+		
+		
+		JPAQuery<Tuple> jpaQuery = query.select(categoryQBean, 
+				JPAExpressions.select(numberExpression)
+					.from(qVideoCategory)
+					.where(qVideoCategory.categoryId.eq(qCategory.id)))
+				.from(qCategory);
+		
+		* 条件, count检索
+
 	# 结果集子查询
 		//TODO
 	
