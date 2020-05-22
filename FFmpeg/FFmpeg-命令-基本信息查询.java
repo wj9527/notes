066915b5209e144dfb7a9cff4d330ca8f993a834
KevinBlandy <747692844@qq.com>
2020-05-22 14:15:47 +0800

    ignore

---
+++ FFmpeg/FFmpeg-命令-基本信息查询.java
@@ -0,0 +1,51 @@
+----------------------
+基本信息查询
+----------------------
+	-version
+		* 显示版本
+	
+	-demuxers 
+		* 显示可用的 demuxers
+	
+	-muxers
+		* 显示可用的 muxers
+	
+	-devices
+		* 显示可用的设备
+	
+	-codecs
+		* 显示可用的编解码器
+	
+	-decoders
+		* 显示可用的解码器
+	
+	-encoders
+		* 显示可用的编码器
+	
+	-bsfs
+		* 显示比特流filter
+	
+	-formats
+		* 显示可用的格式
+	
+	-protocols
+		* 显示可用的协议
+	
+	-filters
+		* 显示可用的过滤器
+	
+	-pix_fmts
+		* 显示可用的像素格式
+	
+	-sample_fmts
+		* 显示可用的采样格式
+	
+	-layouts
+		* 显示channel名称
+	
+	-colors
+		* 显示识别的颜色名称
+	
+
+	
+	
