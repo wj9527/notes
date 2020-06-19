bc6ef70f5667ac46a84506353b1294fa943ee355
KevinBlandy <747692844@qq.com>
2020-06-19 17:03:23 +0800

    ignore

--- Python/python-模块-并发/python-concurrent.py
+++ Python/python-模块-并发/python-concurrent.py
@@ -3,12 +3,12 @@ concurrent				 |
 -------------------------
 	* 这是一个面向并发的包
 	* 里面有很多面向并发的类库
-		concurrent.futures.ThreadPoolExecutor
+		concurrent.futures.thread.ThreadPoolExecutor
 			* 线程池
 
 
 ------------------------------------------------
-concurrent.futures.ThreadPoolExecutor			|
+concurrent.futures.thread.ThreadPoolExecutor			|
 ------------------------------------------------
 	* 线程池类
 	* 构造函数
@@ -26,16 +26,10 @@ concurrent.futures.ThreadPoolExecutor			|
 			* fn:就是任务函数
 			* *args, **kwargs 是运行该函数时,传递的参数
 			* 返回值是一个 Future 对象
+				add_done_callback()
+					* 添加线程结束,执行的回调函数
+					* 会自动的把当前 future(this),传递给该函数的第一个参数
+					* future.add_done_callback(lambda x: print('线程执行完毕,结果是:%s'%(x.result())))
 
-------------------------------------------------
-concurrent.futures._base.Future					|
-------------------------------------------------
-	* 线程池中,执行线程(submit()),返回的 Future 对象
-	* 实例方法
-		add_done_callback()
-			* 添加线程结束,执行的回调函数
-			* 会自动的把当前 future(this),传递给该函数的第一个参数
-			* future.add_done_callback(lambda x: print('线程执行完毕,结果是:%s'%(x.result())))
-
-		result()
-			* 获取线程执行完毕后,返回(return)的数据
+				result()
+					* 获取线程执行完毕后,返回(return)的数据
