b483b359f17affd5489f04310bf82261541719af
KevinBlandy <747692844@qq.com>
2020-06-22 14:53:11 +0800

    ignore

---
+++ Shell/shell-练习.sh
@@ -0,0 +1,16 @@
+-------------------
+ц╟ещеепР
+-------------------
+#!/bin/bash
+arr=(4 7 5 2 3 6 8 1 0 9)
+length=${#arr[*]}
+for (( i = 0; i < ${length}; i++ )); do
+  for (( y = ${i} + 1; y < ${length}; y++ )); do
+  	if (( ${arr[i]} > ${arr[y]} )); then
+  	    temp=${arr[i]}
+		arr[${i}]=${arr[y]}
+		arr[${y}]=${temp}
+  	fi
+  done
+done
+echo ${arr[*]}
