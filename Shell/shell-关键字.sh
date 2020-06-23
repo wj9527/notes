9991b6202ecb883222be5536b359567c66463a2d
KevinBlandy <747692844@qq.com>
2020-06-23 21:40:02 +0800

    ignore

--- Shell/shell-关键字.sh
+++ Shell/shell-关键字.sh
@@ -37,7 +37,34 @@
 
 	read
 		读标准输入 
+	
+	source 
+		* 执行另一个脚本, 通常用于重新加载一个配置文件。
+		* 最大的特点是在当前 Shell 执行脚本, 不像直接执行脚本时，会新建一个子 Shell
+		* 所以, source命令执行脚本时, 不需要export变量
 
+		* 可以通过这方式导入其他外部脚本的变量, 函数
+
+		* 简写, 可以使用一个点（.）来表示
+			. .bashrc
+
+	alias 
+		* 为一个命令指定别名
+			alias search=grep # 为grep命令起一个search的别名。
+
+		* 可以用来为长命令指定一个更短的别名
+			$ alias today='date +"%A, %B %-d, %Y"'
+			$ today # Tuesday, June 23, 2020
+
+		* 为了防止误删除文件，可以指定rm命令的别名
+			alias rm='rm -i'
+		* 可以接受参数，参数会直接传入原始命令
+		* 直接调用alias命令，可以显示所有别名
+			alias
+		
+		* unalias命令可以解除别名
+			unalias lt
+		
 	expr
 		对整数型变量进行算术运算 
 
